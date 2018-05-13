71be6c8767c1cced375c298d31b7941088533504
Andy Sigler <andrewsigler1@gmail.com>
2018-05-13 17:22:58 -0400

    create Gcode class for parsing/sending serial commands

--- modules/temp-deck/temp-deck-arduino/gcode.cpp
+++ modules/temp-deck/temp-deck-arduino/gcode.cpp
@@ -1,19 +1,17 @@
 #include "Gcode.h"
 
 Gcode::Gcode() {
-    Serial.begin(115200);
-    Serial.setTimeout(2);
+    COMMAND_CODES[0] = GCODE_GET_TEMP;
+    COMMAND_CODES[1] = GCODE_SET_TEMP;
+    COMMAND_CODES[2] = GCODE_DEVICE_INFO;
+    COMMAND_CODES[3] = GCODE_DISENGAGE;
+    COMMAND_CODES[4] = GCODE_DFU;
 }
 
-void Gcode::_send_ack(){
-    Serial.println("ok");
-    Serial.println("ok");
-}
-
-void Gcode::_strip_serial_buffer(){
+void Gcode::_strip_serial_buffer() {
   for(uint8_t i=0;i<SERIAL_BUFFER_STRING.length();i++) {
     for (uint8_t c=0;c<sizeof(CHARACTERS_TO_STRIP);c++) {
-      if(SERIAL_BUFFER_STRING[i] == CHARACTERS_TO_STRIP[c]){
+      if(SERIAL_BUFFER_STRING[i] == CHARACTERS_TO_STRIP[c]) {
         SERIAL_BUFFER_STRING.remove(i, 1);
         i--;
         break;
@@ -22,88 +20,99 @@ void Gcode::_strip_serial_buffer(){
   }
 }
 
-void Gcode::_parse_temp_from_buffer() {
-  int starting_s_index = SERIAL_BUFFER_STRING.indexOf('S');
-  if (starting_s_index >= 0) {
+bool Gcode::pop_command() {
+  code = "";
+  while (GCODE_BUFFER_STRING.length()) {
+    for (uint8_t i=1;i<TOTAL_GCODE_COMMAND_CODES;i++) {
+      if (GCODE_BUFFER_STRING.substring(0, COMMAND_CODES[i].length()) == COMMAND_CODES[i]) {
+        GCODE_BUFFER_STRING.remove(0, COMMAND_CODES[i].length());
+        code = COMMAND_CODES[i];
+        return true;
+      }
+    }
+    GCODE_BUFFER_STRING.remove(0, 1);
+  }
+  return false;
+}
+
+bool Gcode::received_newline() {
+  if (Serial.available()){
+    if (SERIAL_BUFFER_STRING.length() > MAX_SERIAL_BUFFER_LENGTH) {
+      SERIAL_BUFFER_STRING = "";
+    }
+    SERIAL_BUFFER_STRING += Serial.readStringUntil("\r\n");
+    if (SERIAL_BUFFER_STRING.charAt(SERIAL_BUFFER_STRING.length() - 2) == '\r') {
+      if (SERIAL_BUFFER_STRING.charAt(SERIAL_BUFFER_STRING.length() - 1) == '\n') {
+        _strip_serial_buffer();
+        GCODE_BUFFER_STRING += SERIAL_BUFFER_STRING;
+        SERIAL_BUFFER_STRING = "";
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void Gcode::send_ack() {
+  Serial.println("ok");
+  Serial.println("ok");
+}
+
+boolean Gcode::read_int(char key) {
+  parsed_int = 0;
+  int starting_key_index = GCODE_BUFFER_STRING.indexOf(key);
+  if (starting_key_index >= 0) {
     String parsed_number = "";
     char next_char;
     boolean valid_command = false;
-    for (uint8_t i=1;i<4;i++){
-      if (SERIAL_BUFFER_STRING.length() <= starting_s_index + i){
+    for (uint8_t i=1;i<4;i++) {
+      if (GCODE_BUFFER_STRING.length() <= starting_key_index + i) {
         break;
       }
-      next_char = SERIAL_BUFFER_STRING.charAt(starting_s_index + i);
-      if (next_char >= '0' && next_char <= '9') {
-        if (i == 3) {
-          valid_command = false;  // number sent is 3 digits, so ignore it
-          break;
-        }
+      next_char = GCODE_BUFFER_STRING.charAt(starting_key_index + i);
+      if ((next_char >= '0' && next_char <= '9') || next_char == '-') {
         parsed_number += next_char;
-        valid_command = true;
       }
       else {
         break;
       }
     }
-    if (valid_command) {
-      target_temp = parsed_number.toInt();
+    if (parsed_number) {
+      parsed_int = parsed_number.toInt();
+      return true;
+    }
+    else {
+      return false;
     }
   }
 }
 
-void Gcode::_print_current_temperature() {
+void Gcode::print_device_info(String serial, String model, String version) {
+  Serial.print("serial:");
+  Serial.print(serial);
+  Serial.print(" model:");
+  Serial.print(model);
+  Serial.print(" version:");
+  Serial.print(version);
+  Serial.println();
+}
+
+void Gcode::print_targetting_temperature(int target_temp, int current_temp) {
   Serial.print("T:");
-  if (target_temp == NO_TARGET_SET) {
-    Serial.print("none");
-  }
-  else {
-    Serial.print(target_temp);
-  }
+  Serial.print(target_temp);
   Serial.print(" C:");
   Serial.println(current_temp);
 }
 
-void Gcode::_print_model_and_version() {
-  Serial.println("model:temp-v1 version:edge-1a2b3c4");
+void Gcode::print_stablizing_temperature(int current_temp) {
+  Serial.print("T:");
+  Serial.print("none");
+  Serial.print(" C:");
+  Serial.println(current_temp);
 }
 
-void Gcode::_parse_gcode_commands() {
-  _strip_serial_buffer();
-  while (SERIAL_BUFFER_STRING.length()) {
-    for (uint8_t i=0;i<TOTAL_GCODE_COMMAND_CODES;i++){
-      if (SERIAL_BUFFER_STRING.substring(0, COMMAND_CODES[i].length()) == COMMAND_CODES[i]) {
-        switch (i) {
-          case 0:  // M105  (get temp)
-            _print_current_temperature();
-            break;
-          case 1:  // M104  (set temp)
-            _parse_temp_from_buffer();
-            break;
-          case 2:  // M18  (OFF)
-            target_temp = NO_TARGET_SET;
-            break;
-          case 3:  // M115  (get model/version)
-            _print_model_and_version();
-            break;
-          case 4:  // dfu  (start bootloader)
-            Serial.println("Entering bootloader");
-            break;
-        }
-      }
-    }
-    SERIAL_BUFFER_STRING.remove(0, 1);
-  }
-  SERIAL_BUFFER_STRING = "";
-}
-
-uint8_t Gcode::update(){
-  if (Serial.available()){
-    SERIAL_BUFFER_STRING += Serial.readStringUntil("\r\n");
-    if (SERIAL_BUFFER_STRING.charAt(SERIAL_BUFFER_STRING.length() - 2) == '\r') {
-      if (SERIAL_BUFFER_STRING.charAt(SERIAL_BUFFER_STRING.length() - 1) == '\n') {
-        _parse_gcode_commands();
-        _send_ack();
-      }
-    }
-  }
+void Gcode::setup(int baudrate) {
+  Serial.begin(baudrate);
+  Serial.setTimeout(2);
+  Seiral.println("Temp-Deck Starting")
 }
--- modules/temp-deck/temp-deck-arduino/gcode.h
+++ modules/temp-deck/temp-deck-arduino/gcode.h
@@ -1,41 +1,79 @@
+/*
+
+
+Gcode gcode = Gcode(115200);
+
+if (gcode.received_newline()) {
+    while (gcode.pop_command()) {
+        switch(gcode.code) {
+            case GCODE_GET_TEMP:
+                gcode.print_targetting_temperature(target, current);
+                break;
+            case GCODE_SET_TEMP:
+                if (gcode.parse_int('S')) {
+                    target = gcode.parsed_int;
+                }
+                break;
+            case GCODE_DISENGAGE:
+                off()
+                break;
+            case GCODE_DEVICE_INFO:
+                gcode.print_device_info(serial, model, version);
+                break;
+            case GCODE_DFU:
+                start_dfu_timeout();
+                break;
+            default:
+                break;
+        }
+    }
+    gcode.send_ack();
+}
+
+*/
+
 #ifndef Gcode_h
 #define Gcode_h
 
 #include "Arduino.h"
 
-#define NO_TARGET_SET 200
-#define ROOM_TEMPERATURE 25
+#define NO_TARGET_TEMP_SET 32766
 
-#define TOTAL_GCODE_COMMAND_CODES 5
+#define MAX_SERIAL_BUFFER_LENGTH 500
 
+#define GCODE_GET_TEMP              "M105"
+#define GCODE_SET_TEMP              "M104"
+#define GCODE_DISENGAGE             "M18"
+#define GCODE_DEVICE_INFO           "M115"
+#define GCODE_DFU                   "dfu"
+#define TOTAL_GCODE_COMMAND_CODES   5
 
 class Gcode{
+
     public:
 
         Gcode();
-        void update();
+        void setup(int baudrate);
+        bool received_newline();
+        void pop_command();
+        String code;
+        int parsed_int;
+        void print_device_info(String serial, String model, String version);
+        void print_targetting_temperature(int target_temp, int current_temp);
+        void print_stablizing_temperature(int current_temp);
+        void read_int(char key);
+        String code;
 
     private:
 
-        String COMMAND_CODES[TOTAL_GCODE_COMMAND_CODES] = {
-          "M105", "M104", "M18", "M115", "dfu"
-        };
-
+        String COMMAND_CODES[TOTAL_GCODE_COMMAND_CODES];
+        String GCODE_BUFFER_STRING;
         String SERIAL_BUFFER_STRING;
         char CHARACTERS_TO_STRIP[3] = {' ', '\r', '\n'};
+        bool SEND_ACK_NEXT_UPDATE = false;
 
-        int target_temp = NO_TARGET_SET;
-        int current_temp = ROOM_TEMPERATURE;
-
-        const int MILLIS_PER_DEGREE = 100;
-        unsigned long degree_change_timestamp = 0;
-        unsigned long now = 0;
-
-        void _send_ack();
+        void _send_ack_if_flagged();
         void _strip_serial_buffer();
-        void _parse_temp_from_buffer();
-        void _print_current_temperature();
-        void _print_model_and_version();
         void _parse_gcode_commands();
 };
 
