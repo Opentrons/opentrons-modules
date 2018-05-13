e8db09545b1b9dfd9e5f9dbf2be4e7dd89eba1e2
Andy Sigler <andrewsigler1@gmail.com>
2018-05-13 17:23:57 -0400

    uses Gcode interface in main

--- modules/temp-deck/temp-deck-arduino/temp-deck-arduino.ino
+++ modules/temp-deck/temp-deck-arduino/temp-deck-arduino.ino
@@ -6,27 +6,43 @@
 #include "lights.h"
 #include "peltiers.h"
 #include "thermistor.h"
+#include "gcode.h"
 
 Lights lights = Lights();
 Peltiers peltiers = Peltiers();
 Thermistor thermistor = Thermistor();
+Gcode gcode = Gcode(115200);
 
-#define tone_out 11
+#define pin_tone_out 11
+#define pin_fan_pwm 9
 
-#define fan_pwm 9
+#define TEMPERATURE_MAX 99
+#define TEMPERATURE_MIN -9
 
 int prev_temp = 0;
 
 int TARGET_TEMPERATURE = 25;
 boolean IS_TARGETING = false;
 
+String device_serial = "TD001180622A01"
+String device_model = "001"
+String device_version = "edge-1a2b3c4"
+
 /////////////////////////////////
 /////////////////////////////////
 /////////////////////////////////
 
 void set_target_temperature(int target_temp){
-  if (target_temp < 0) target_temp = 0;
-  if (target_temp > 99) target_temp = 99;
+  if (target_temp < TEMPERATURE_MIN) {
+    target_temp = TEMPERATURE_MIN;
+    gcode.print_warning(
+      "Target temperature too low, setting to TEMPERATURE_MIN degrees");
+  }
+  if (target_temp > TEMPERATURE_MAX) {
+    target_temp = TEMPERATURE_MAX;
+    gcode.print_warning(
+      "Target temperature too high, setting to TEMPERATURE_MAX degrees");
+  }
   IS_TARGETING = true;
   TARGET_TEMPERATURE = target_temp;
 }
@@ -58,7 +74,7 @@ void delay_minutes(int minutes){
 void set_fan_percentage(float percentage){
   if (percentage < 0) percentage = 0;
   if (percentage > 1) percentage = 1;
-  analogWrite(fan_pwm, int(percentage * 255.0));
+  analogWrite(pin_fan_pwm, int(percentage * 255.0));
 }
 
 /////////////////////////////////
@@ -82,7 +98,7 @@ void hot(float amount) {
 void update_target_temperature(){
   peltiers.update_peltier_cycle();
   if (IS_TARGETING) {
-    float temp = thermistor.peltier_temperature();
+    float temp = thermistor.plate_temperature();
     // if we've arrived, just be calm, but don't turn off
     if (int(temp) == TARGET_TEMPERATURE) {
       lights.set_color_bar(0, 1, 0, 0);
@@ -105,17 +121,25 @@ void update_target_temperature(){
 /////////////////////////////////
 
 void update_temperature_display(boolean force=false){
-  int current_temp = thermistor.peltier_temperature();
+  int current_temp = thermistor.plate_temperature();
   boolean stable = true;
 //  for (int i=0;i<10;i++){
-//    if (int(thermistor.peltier_temperature()) != current_temp) {
+//    if (int(thermistor.plate_temperature()) != current_temp) {
 //      stable = false;
 //      break;
 //    }
 //  }
   if (stable || force) {
     if (current_temp != prev_temp || force){
-      lights.display_number(current_temp);
+      if (number > 99){
+        lights.show_message_low();
+      }
+      else if (number < 0){
+        lights.show_message_high();
+      }
+      else {
+        lights.display_number(current_temp);
+      }
     }
     prev_temp = current_temp;
   }
@@ -125,29 +149,8 @@ void update_temperature_display(boolean force=false){
 /////////////////////////////////
 /////////////////////////////////
 
-boolean DOING_CYCLE_TEST = false;
-
-const int cycle_high = 90;
-const int cycle_low = 5;
-
-void cycle_test(){
-  set_fan_percentage(1.0);
-  if (TARGET_TEMPERATURE == int(thermistor.peltier_temperature())){
-    if (TARGET_TEMPERATURE == cycle_high){
-      set_target_temperature(cycle_low);
-    }
-    else {
-      set_target_temperature(cycle_high);
-    }
-  }
-}
-
-/////////////////////////////////
-/////////////////////////////////
-/////////////////////////////////
-
 void activate_bootloader(){
-  // Need to verify if a reset to application code 
+  // Need to verify if a reset to application code
   // is performed properly after DFU
   cli();
   // disable watchdog, if enabled
@@ -156,72 +159,70 @@ void activate_bootloader(){
   USBCON = (1<<FRZCLK);  // disable USB
   UCSR1B = 0;
   _delay_ms(5);
-  #if defined(__AVR_ATmega32U4__)              
+  #if defined(__AVR_ATmega32U4__)
       EIMSK = 0; PCICR = 0; SPCR = 0; ACSR = 0; EECR = 0; ADCSRA = 0;
       TIMSK0 = 0; TIMSK1 = 0; TIMSK3 = 0; TIMSK4 = 0; UCSR1B = 0; TWCR = 0;
       DDRB = 0; DDRC = 0; DDRD = 0; DDRE = 0; DDRF = 0; TWCR = 0;
       PORTB = 0; PORTC = 0; PORTD = 0; PORTE = 0; PORTF = 0;
       asm volatile("jmp 0x3800");   //Bootloader start address
-  #endif 
+  #endif
 }
+
 /////////////////////////////////
 /////////////////////////////////
 /////////////////////////////////
 
-void read_serial(){
-  if (Serial.available()){
-    char letter = Serial.read();
-    int val;
-    if (letter == 'c'){
-      val = Serial.parseInt();
-      cold(float(val) / 100.0);
-      disable_target();
+void disengage() {
+  peltiers.disable_peltiers();
+  lights.set_color_bar(0, 0, 0, 1);
+  disable_target();
+  set_fan_percentage(0.0);
+}
+
+void print_temperature() {
+  if (IS_TARGETING) {
+    gcode.print_stablizing_temperature(
+      thermistor.plate_temperature()
+    );
+  }
+  else {
+    gcode.print_targetting_temperature(
+      TARGET_TEMPERATURE,
+      thermistor.plate_temperature()
+    );
+  }
+}
+
+/////////////////////////////////
+/////////////////////////////////
+/////////////////////////////////
+
+void read_gcode(){
+  if (gcode.received_newline()) {
+    while (gcode.pop_command()) {
+      switch(gcode.code) {
+        case GCODE_GET_TEMP:
+          print_temperature();
+          break;
+        case GCODE_SET_TEMP:
+          if (gcode.parse_int('S')) {
+            set_target_temperature(gcode.parsed_int);
+          }
+          break;
+        case GCODE_DISENGAGE:
+          disengage()
+          break;
+        case GCODE_DEVICE_INFO:
+          gcode.print_device_info(device_serial, device_model, device_version);
+          break;
+        case GCODE_DFU:
+          start_dfu_timeout();
+          break;
+        default:
+          break;
+      }
     }
-    else if (letter == 'h'){
-      val = Serial.parseInt();
-      hot(float(val) / 100.0);
-      disable_target();
-    }
-    else if (letter == 'x'){
-      DOING_CYCLE_TEST = false;
-      peltiers.disable_peltiers();
-      lights.set_color_bar(0, 0, 0, 1);
-      disable_target();
-      set_fan_percentage(0.0);
-    }
-    else if (letter == 'f'){
-      val = Serial.parseInt();
-      set_fan_percentage(float(val) / 100.0);
-    }
-    else if (letter == 't'){
-      val = Serial.parseInt();
-      set_target_temperature(val);
-    }
-    else if (letter == 'p') {
-      Serial.print(thermistor.average_adc());
-      Serial.print("\t");
-      Serial.print(thermistor.peltier_temperature());
-      Serial.println(" deg-C");
-    }
-    else if (letter == 'l'){
-      lights.startup_color_animation();
-    }
-    else if (letter == 'r') {
-      DOING_CYCLE_TEST = true;
-      set_target_temperature(cycle_low);
-    }
-    else if (letter == 'u') {
-      //Update firmware
-      //TODO Send proper ACK
-      Serial.println("Entering Bootloader");
-      delay(2000);
-      activate_bootloader();
-    }
-    while (Serial.available() > 0) {
-      Serial.read();
-    }
-    Serial.println("ok");
-    Serial.println("ok");
+    gcode.send_ack();
   }
 }
 
@@ -230,9 +231,10 @@ void read_serial(){
 /////////////////////////////////
 
 void setup() {
-  pinMode(tone_out, OUTPUT);
-  pinMode(fan_pwm, OUTPUT);
+  pinMode(pin_tone_out, OUTPUT);
+  pinMode(pin_fan_pwm, OUTPUT);
 
+  gcode.setup();
   peltiers.setup_peltiers();
   set_fan_percentage(0);
   lights.setup_lights();
@@ -249,7 +251,7 @@ void setup() {
 /////////////////////////////////
 
 void loop(){
-  read_serial();
+  read_gcode();
   update_temperature_display();
   update_target_temperature();
   if (DOING_CYCLE_TEST){
