14f9d9986eccd2c93cb52deed2a01163ec0b30d3
andySigler <andrewsigler1@gmail.com>
2018-09-07 17:07:27 -0400

    feat(qc): automates ID writing and firmware uploading at factory

--- eepromWriter/write_module_memory.py
+++ eepromWriter/write_module_memory.py
@@ -1,3 +1,14 @@
+'''
+    1) discover port name
+    2) open/close port at 1200bps to reset into bootloader
+    3) Run AVR commnad to upload eeprom writing sketch
+    4) Save ID and model from barcode
+    5) open/close port at 1200bps to reset into bootloader
+    6) Run AVR commnad to upload application firmware
+    7) Check to make sure it return the correct information (M115)
+'''
+
+import subprocess
 import sys
 
 from serial import Serial
@@ -5,8 +16,6 @@ from serial.tools.list_ports import comports
 
 
 OPENTRONS_VID = 1240
-
-port = ''
 
 BAD_BARCODE_MESSAGE = 'Unexpected Serial -> {}'
 WRITE_FAIL_MESSAGE = 'Data not saved'
@@ -16,8 +25,14 @@ MODELS = {
     'MDV01': 'mag_deck_v1.1'
 }
 
+EEPROM_FIRMARE_PATH = './eepromWriter.hex'
+TEMP_DECK_FIRMARE_PATH = './temp-deck-arduino.ino.tempdeck32u4.hex'
+MAG_DECK_FIRMARE_PATH = './mag-deck-arduino.ino.magdeck32u4.hex'
 
-def connect_to_module():
+AVR_COMMAND = 'avrdude -C ./avrdude.conf -v -patmega32u4 -cavr109 -P {port} -b 57600 -D -U flash:w:{firmware}:i'  # NOQA
+
+
+def find_device_port():
     port = None
     for p in comports():
         if p.vid == OPENTRONS_VID:
@@ -25,10 +40,52 @@ def connect_to_module():
             break
     if port is None:
         raise RuntimeError('Could not find Opentrons Model connected over USB')
-    print()
-    print('Connecting to module...')
-    td = Serial(port, 115200, timeout=1)
+    return port
+
+
+def trigger_bootloader(port_name):
+    port = Serial(port_name, 1200, timeout=1)
+    time.sleep(0.02)
+    port.close()
     return td
+
+
+def upload_eeprom_sketch(port_name):
+    cmd = AVR_COMMAND.format(
+        port=port_name, firmware=EEPROM_FIRMARE_PATH)
+    res = subprocess.check_output('{} &> /dev/null'.format(cmd), shell=True)
+
+
+def upload_application_firmware(port_name, model):
+    hex_file = ''
+    if 'temp' in model:
+        hex_file = TEMP_DECK_FIRMARE_PATH
+    elif 'mag' in model:
+        hex_file = MAG_DECK_FIRMARE_PATH
+    else raise RuntimeError(
+        'Unknown model for writing firmware: {}'.format(model))
+    cmd = AVR_COMMAND.format(
+        port=port_name, firmware=hex_file)
+
+
+def assert_id_and_model(port, serial, model):
+    msg = 'M115\r\n'.encode()
+    port.reset_input_buffer()
+    port.write(msg)
+    res = port.read_until('ok\r\nok\r\n'.encode())
+    res = res.decode().split('\r')[0]
+    device_info = {
+        r.split(':')[0]: r.split(':')[1]
+        for r in res.split(' ')
+    }
+    assert device_info.get('serial') == barcode
+    assert device_info.get('model') == model
+
+
+def connect_to_module(port_name):
+    print('Connecting to module...')
+    port = Serial(port_name, 115200, timeout=1)
+    return port
 
 
 def write_identifiers(module, new_id, new_model):
@@ -43,7 +100,7 @@ def write_identifiers(module, new_id, new_model):
 def check_previous_data(module):
     serial, model = _get_info(module)
     if not serial or not model:
-        print('No old data on this pipette')
+        print('No old data on this device')
         return
     print(
         'Overwriting old data: id={0}, model={1}'.format(serial, model))
@@ -83,19 +140,30 @@ def _parse_model_from_barcode(barcode):
 
 def main():
     print('\n')
+    connected_port = None
     try:
+        port_name = find_device_port()
+        trigger_bootloader(port_name)
+        upload_eeprom_sketch(port_name)
         barcode = _user_submitted_barcode(32)
         model = _parse_model_from_barcode(barcode)
-        module = connect_to_module()
-        check_previous_data(module)
-        write_identifiers(module, barcode, model)
-        module.close()
+        connected_port = connect_to_module(port_name)
+        check_previous_data(connected_port)
+        write_identifiers(connected_port, barcode, model)
+        connected_port.close()
+        trigger_bootloader(port_name)
+        upload_application_firmware(port_name, model)
+        connected_port = connect_to_module(port_name)
+        assert_id_and_model(connected_port, barcode, model)
         print('PASS: Saved -> {0} (model {1})'.format(barcode, model))
     except KeyboardInterrupt:
         exit()
     except Exception as e:
         print('FAIL: {}'.format(e))
-    main()
+    finally:
+        if connected_port:
+            connected_port.close()
+        main()
 
 
 if __name__ == "__main__":
