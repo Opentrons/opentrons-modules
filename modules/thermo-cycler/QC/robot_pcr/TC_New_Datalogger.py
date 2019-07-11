550881ba19cbc665fd49af90620672f177201caa
Sanniti <sanniti.p@nyu.edu>
2019-07-11 13:57:24 -0400

    get ip from file args

--- modules/thermo-cycler/QC/robot_pcr/TC_New_Datalogger.py
+++ modules/thermo-cycler/QC/robot_pcr/TC_New_Datalogger.py
@@ -4,17 +4,17 @@ import csv
 import time
 from argparse import ArgumentParser
 
-IP = "10.10.3.151"
 TC_SERIAL = "dummySerial"
 
 def build_arg_parser():
     arg_parser = ArgumentParser(
         description="Thermocycler temperature data logger")
     arg_parser.add_argument("-F", "--csv_file_name", required=True)
+    arg_parser.add_argument("-IP", "--robot_ip_address", required=True)
     return arg_parser
 
-async def fetch(client):
-    async with client.get(f'http://{IP}:31950/modules/{TC_SERIAL}/data') as resp:
+async def fetch(client, ip_addr):
+    async with client.get(f'http://{ip_addr}:31950/modules/{TC_SERIAL}/data') as resp:
         assert resp.status == 200
         return await resp.json()
 
@@ -27,7 +27,7 @@ async def main():
         data_writer.writerow(["Time", "Lid Current", "Plate Current", "Plate Target", "Hold countdown"])
     while True:
         async with aiohttp.ClientSession() as client:
-            data = await fetch(client)
+            data = await fetch(client, args.robot_ip_address)
             with open('{}.csv'.format(args.csv_file_name), mode='a') as data_file:
                 data_writer = csv.writer(data_file, delimiter=',', quoting=csv.QUOTE_NONE)
                 res = time.localtime()
