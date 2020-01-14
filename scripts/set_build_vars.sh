7b2d92fb313d853d208a722c9e2dba002696f5a3
Brian Cooper <brian@allegro.com>
2020-01-14 17:37:56 -0500

    chore(CI): rework deploy process

---
+++ scripts/set_build_vars.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BUILDS_DIR="./build"
+
+RELEASE_VERSION=$(cut -d'@' -f2 <<< $TRAVIS_TAG)
+THIS_BUILD_TAG=$(git describe --tags)
+
+case $TRAVIS_TAG in
+
+  tempdeck@v*)
+    export RELEASE_LOCAL_DIR="${BUILDS_DIR}/temp-deck"
+    export RELEASE_UPLOAD_DIR="temperature-module/${RELEASE_VERSION}"
+    ;;
+
+  magdeck@v*)
+    export RELEASE_LOCAL_DIR="${BUILDS_DIR}/mag-deck"
+    export RELEASE_UPLOAD_DIR="magnetic-module/${RELEASE_VERSION}"
+    ;;
+
+  thermocycler@v*)
+    export RELEASE_LOCAL_DIR="${BUILDS_DIR}/thermo-cycler"
+    export RELEASE_UPLOAD_DIR="thermocycler/${RELEASE_VERSION}"
+    ;;
+
+  *)
+    export RELEASE_LOCAL_DIR=$BUILDS_DIR
+    export RELEASE_UPLOAD_DIR="modules-${THIS_BUILD_TAG}-${TRAVIS_BRANCH}"
+    ;;
+
+esac
