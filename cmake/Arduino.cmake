4a29e5a15d3debdd4074c51524ac42a868ca63d9
Seth Foster <seth@opentrons.com>
2021-02-03 18:06:11 -0500

    Add a bunch more docs

--- cmake/Arduino.cmake
+++ cmake/Arduino.cmake
@@ -1,3 +1,74 @@
+#[=======================================================================[.rst:
+Arduino
+-------
+
+Provides a function to replace ``add_executable`` (sadly, not ``target_add_executable``,
+since it works using add_custom_command) to build an arduino sketch using the arduino IDE's
+command-line interface specific to that project with the proper boards and so on.
+
+It is necessary because the Arduino ide only accepts inputs that end in ``.ino``. That means
+that CMake's built-in compiler sensing, which actually works pretty well with most compilation
+setups now that you can tell it to not try to link anything, cannot possibly work and it gets
+very sad. So we kind of opt out of the normal CMake compiler machinery and resort to use of
+``add_custom_command``.
+
+This module provides the function ``add_arduino_command``
+
+add_arduino_command
+^^^^^^^^^^^^^^^^^^^
+
+``version_for_project`` uses ``add_custom_command`` to add a build system target to compile
+an arduino-based module's sketch with proper dependency management, and ``add_custom_target``
+to make it available with a nice alias.
+
+Input Variables
++++++++++++++++
+
+All input variables are by keyword (they are parsed using ``cmake_parse_arguments``).
+
+``TARGET_NAME`` Mandatory keyword argument, single-valued. Specifies the name of the target that will
+be created; it can be used after the invocation of ``add_arduino_command`` in, for instance,
+an ``add_dependencies`` call.
+
+``SKETCH`` Mandatory keyword argument, single-valued. The full path to the sketch (the file whose name
+ends in ``.ino``) to compile.
+
+``FQBN`` Mandatory keyword argument, single-valued. The fully qualified board name for the arduino IDE
+to use.
+
+``OUTPUT_FORMAT`` Mandatory keyword argument, single-valued. Should be either `hex` or `bin`. Used to
+find the ``objcopy``d output of the compile. For instance, if ``hex``, the system will look for
+``${SKETCH_NAME}.hex``.
+
+``WORKING_DIRECTORY`` Optional keyword argument, single-valued. If specified, passed directly to
+``add_custom_command``. If not specified, the default is ``CMAKE_CURRENT_BINARY_DIR``.
+
+``PROJECT_FILES`` Optional keyword argument, list-valued. If specified, passed as dependencies to
+``add_custom_command``.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define a target called ``${TARGET_NAME}``, and a file suitable for use as a dependency called
+``${SKETCH_NAME}.${OUTPUT_FORMAT}``.
+
+Other Factors
+^^^^^^^^^^^^^
+
+Extra compiler defines (passed with ``-D``) for the arduino compiler are read from the file properties
+of the sketch file (specified in ``SKETCH_FILE``). This is an ugly workaround. To get it to work, you
+must
+
+- Make sure the _name_ of the variable is in a list in the property ``ARDUINO_DEFINES``
+- Set a property with that name and the value on the file
+
+There is some (fairly rough) type analysis so that TRUE or FALSE-valued defines will end up as 1 or 0,
+numerical (``^[0-9]+$``)-valued defines will end up in there literally, and everything else will be
+enclosed by quotes.
+
+
+#]=======================================================================]
+
 
 function(ADD_ARDUINO_COMMAND)
   set(ONE_VALUE_ARGS TARGET_NAME SKETCH FQBN WORKING_DIRECTORY OUTPUT_FORMAT)
--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -1,10 +1,27 @@
-# This is a toolchain file to use the arduino IDE headless to build
-# firmware. It also downloads the Arduino IDE if it cannot be found.
+#[=======================================================================[.rst:
+ArduinoToolchain
+----------------
+
+This module should never be imported and used in a CMakeLists.txt file at either
+build or configuration time. It is only intended to be used as a toolchain specified
+either in a preset or with the ``-DCMAKE_TOOLCHAIN_FILE`` command-line option.
+
+It will download the appropriate arduino IDE for the current system to
+``opentrons-modules/arduino-ide/SYSTEM_NAME`` and set cache variables accordingly.
+
+It also configures the IDE with some settings that would be a little annoying to set
+every time we build.
+
+Because this all works once and is gated on downloading the IDE again, if you ever
+need to redownload it you should ``rm -rf ./arduino_ide`` and then rerun ``cmake``.
+#]=======================================================================]
+
 
 message(STATUS "Setting arduino toolchain")
 include(FetchContent)
 set(ARDUINO_IDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../arduino_ide")
-message(STATUS "System name ${CMAKE_HOST_SYSTEM_NAME}")
+
+# Figure out the appropriate archive url tail
 if("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Linux")
   set(ARDUINO_ARCHIVE
     "arduino-${ARDUINO_IDE_VERSION}-linux64.tar.xz")
@@ -16,8 +33,7 @@ else()
     "arduino-${ARDUINO_IDE_VERSION}-windows.zip")
 endif()
 
-message(STATUS ${ARDUINO_ARCHIVE})
-
+# Set up the fetch
 FetchContent_Declare(
   ARDUINO_IDE
   PREFIX ${ARDUINO_IDE_DIR}
@@ -26,6 +42,11 @@ FetchContent_Declare(
   URL "https://downloads.arduino.cc/${ARDUINO_ARCHIVE}"
   )
 
+# This macro means we have to type this less, and we'll be calling it a lot
+# since we a) want to find the IDE b) want to be OK with not finding it the
+# first time, since the point is to download it and c) want to require finding
+# it subsequent times. REQUIRED here is an optionally-present argument that
+# should be set to either the string REQUIRED, or empty (this is why it's a macro).
 macro(FIND_ARDUINO REQUIRED)
   find_program(
     ARDUINO_EXECUTABLE
@@ -38,13 +59,18 @@ macro(FIND_ARDUINO REQUIRED)
     )
 endmacro()
 
+# First find: figure out if we need to download
 find_arduino("")
 
 if(${ARDUINO_EXECUTABLE} STREQUAL "ARDUINO_EXECUTABLE-NOTFOUND")
   message(STATUS "Didn't find arduino, downloading")
+  # Actually download the IDE
   FetchContent_Populate(ARDUINO_IDE)
+
+  # Second find: actually find the executable so we can configure it
   find_arduino("")
 
+  # Run some configurations that make the IDE available
   execute_process(
     COMMAND ${ARDUINO_EXECUTABLE} --pref "sketchbook.path=${CMAKE_CURRENT_LIST_DIR}/../arduino-modules" --save-prefs
     COMMAND ${ARDUINO_EXECUTABLE} --pref "boardsmanager.additional.urls=https://s3.us-east-2.amazonaws.com/opentrons-modules/package_opentrons_index.json" --save-prefs
@@ -55,9 +81,11 @@ if(${ARDUINO_EXECUTABLE} STREQUAL "ARDUINO_EXECUTABLE-NOTFOUND")
 
 endif()
 
-message(STATUS "current list dir is ${CMAKE_CURRENT_LIST_DIR}")
+# Third (or second, depending how it went) find: throw an error if something failed
 find_arduino(REQUIRED)
 
+# Cross-compile setup. Typically you'd set CMAKE_CXX_COMPILER here, but that would cause cmake to try and
+# run something with try_compile, and that would fail since the arduino_ide isn't actually a CXX compiler.
 set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_SYSTEM_VERSION 0)
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
