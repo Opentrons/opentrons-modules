457e66a9d1fb0e579f7a24056184c46d145d4c1d
Seth Foster <seth@opentrons.com>
2021-04-06 11:11:03 -0400

    format and lint

--- stm32-modules/heater-shaker/firmware/freertos_comms_task.cpp
+++ stm32-modules/heater-shaker/firmware/freertos_comms_task.cpp
@@ -4,8 +4,8 @@
 #include "firmware/freertos_comms_task.hpp"
 
 #include <array>
-#include <utility>
 #include <functional>
+#include <utility>
 
 #include "FreeRTOS.h"
 #include "task.h"
@@ -26,38 +26,18 @@ constexpr size_t MAX_USB_MESSAGE_SIZE_BYTES = 128;
 struct CommsTaskFreeRTOS {
     USBD_CDC_ItfTypeDef cdc_class_fops;
     USBD_HandleTypeDef usb_handle;
+    USBD_CDC_LineCodingTypeDef linecoding;
     std::array<uint8_t, MAX_USB_MESSAGE_SIZE_BYTES> rx_buf;
     std::array<uint8_t, MAX_USB_MESSAGE_SIZE_BYTES> tx_buf;
 };
 
-static int8_t CDC_Init(void);
-static int8_t CDC_DeInit(void);
-static int8_t CDC_Control(uint8_t, uint8_t *, uint16_t);
-static int8_t CDC_Receive(uint8_t*, uint32_t*);
+static auto CDC_Init() -> int8_t;
+static auto CDC_DeInit() -> int8_t;
 
-
-CommsTaskFreeRTOS local_task = {
-  .cdc_class_fops = {
-     .Init = CDC_Init,
-     .DeInit = CDC_DeInit,
-     .Control = CDC_Control,
-     .Receive = CDC_Receive,
-   },
-  .usb_handle = {},
-  .rx_buf = {},
-  .tx_buf = {}
-};
-
-USBD_CDC_LineCodingTypeDef linecoding =
-{
-  115200, /* baud rate*/
-  0x00,   /* stop bits-1*/
-  0x00,   /* parity - none*/
-  0x08    /* nb. of bits 8*/
-};
-
-
-
+// NOLINTNEXTLINE(readability-named-parameter)
+static auto CDC_Control(uint8_t, uint8_t *, uint16_t) -> int8_t;
+// NOLINTNEXTLINE(readability-named-parameter)
+static auto CDC_Receive(uint8_t *, uint32_t *) -> int8_t;
 
 namespace host_comms_control_task {
 
@@ -65,14 +45,35 @@ enum class Notifications : uint8_t {
     INCOMING_MESSAGE = 1,
 };
 
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static FreeRTOSMessageQueue<host_comms_task::Message>
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
     _comms_queue(static_cast<uint8_t>(Notifications::INCOMING_MESSAGE),
                  "Comms Message Queue");
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
+static CommsTaskFreeRTOS _local_task = {
+    .cdc_class_fops =
+        {
+            .Init = CDC_Init,
+            .DeInit = CDC_DeInit,
+            .Control = CDC_Control,
+            .Receive = CDC_Receive,
+        },
+    .usb_handle = {},
+
+    .linecoding =
+        {.bitrate =
+             115200,  // NOLINT(readability-magic-numbers,cppcoreguidelines-avoid-magic-numbers)
+         .format = 0x00,
+         .paritytype = 0x00,
+         .datatype = 0x08},
+    .rx_buf = {},
+    .tx_buf = {},
+};
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static auto _top_task = host_comms_task::HostCommsTask(_comms_queue);
-static auto _local_task = local_task;
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 static auto _tasks = std::make_pair(&_top_task, &_local_task);
 
 static constexpr uint32_t stack_size = 500;
@@ -91,11 +92,11 @@ void run(void *param) {  // NOLINT(misc-unused-parameters)
     auto *local_task = task_pair->second;
     USBD_Init(&local_task->usb_handle, &CDC_Desc, 0);
     USBD_RegisterClass(&local_task->usb_handle, USBD_CDC_CLASS);
-    USBD_CDC_SetTxBuffer(
-        &local_task->usb_handle, local_task->tx_buf.data(), local_task->tx_buf.size());
-    USBD_CDC_SetRxBuffer(
-        &local_task->usb_handle, local_task->rx_buf.data());
-    USBD_CDC_RegisterInterface(&local_task->usb_handle, &local_task->cdc_class_fops);
+    USBD_CDC_SetTxBuffer(&local_task->usb_handle, local_task->tx_buf.data(),
+                         local_task->tx_buf.size());
+    USBD_CDC_SetRxBuffer(&local_task->usb_handle, local_task->rx_buf.data());
+    USBD_CDC_RegisterInterface(&local_task->usb_handle,
+                               &local_task->cdc_class_fops);
     USBD_Start(&local_task->usb_handle);
     while (true) {
         vTaskDelay(delay_ticks);
@@ -114,89 +115,88 @@ auto start()
 }
 }  // namespace host_comms_control_task
 
-
-static int8_t CDC_Init(void)
-{
-  /*
-     Add your initialization code here
-  */
-  return (0);
+static auto CDC_Init() -> int8_t {
+    /*
+       Add your initialization code here
+    */
+    return (0);
 }
 
-
-static int8_t CDC_DeInit(void)
-{
-  /*
-     Add your deinitialization code here
-  */
-  return (0);
+static auto CDC_DeInit() -> int8_t {
+    /*
+       Add your deinitialization code here
+    */
+    return (0);
 }
 
+static auto CDC_Control(uint8_t cmd, uint8_t *pbuf, uint16_t length) -> int8_t {
+    using namespace host_comms_control_task;
+    static_cast<void>(length);
+    switch (cmd) {
+        case CDC_SEND_ENCAPSULATED_COMMAND:  // NOLINT(bugprone-branch-clone)
+            break;
 
+        case CDC_GET_ENCAPSULATED_RESPONSE:
+            break;
 
-static int8_t CDC_Control(uint8_t cmd, uint8_t *pbuf, uint16_t length)
-{
-  switch (cmd)
-  {
-    case CDC_SEND_ENCAPSULATED_COMMAND:
-      /* Add your code here */
-      break;
+        case CDC_SET_COMM_FEATURE:
+            break;
 
-    case CDC_GET_ENCAPSULATED_RESPONSE:
-      /* Add your code here */
-      break;
+        case CDC_GET_COMM_FEATURE:
+            break;
 
-    case CDC_SET_COMM_FEATURE:
-      /* Add your code here */
-      break;
+        case CDC_CLEAR_COMM_FEATURE:
+            break;
 
-    case CDC_GET_COMM_FEATURE:
-      /* Add your code here */
-      break;
+        case CDC_SET_LINE_CODING:
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            _local_task.linecoding.bitrate = (uint32_t)(
+                // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+                pbuf[0] | (pbuf[1] << 8) | (pbuf[2] << 16) | (pbuf[3] << 24));
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            _local_task.linecoding.format = pbuf[4];
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-avoid-magic-numbers)
+            _local_task.linecoding.paritytype = pbuf[5];
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-avoid-magic-numbers)
+            _local_task.linecoding.datatype = pbuf[6];
+            break;
 
-    case CDC_CLEAR_COMM_FEATURE:
-      /* Add your code here */
-      break;
+        case CDC_GET_LINE_CODING:
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            pbuf[0] = (uint8_t)(_local_task.linecoding.bitrate);
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            pbuf[1] = (uint8_t)(_local_task.linecoding.bitrate >> 8);
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            pbuf[2] = (uint8_t)(_local_task.linecoding.bitrate >> 16);
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            pbuf[3] = (uint8_t)(_local_task.linecoding.bitrate >> 24);
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+            pbuf[4] = _local_task.linecoding.format;
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-avoid-magic-numbers)
+            pbuf[5] = _local_task.linecoding.paritytype;
+            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic,cppcoreguidelines-avoid-magic-numbers)
+            pbuf[6] = _local_task.linecoding.datatype;
 
-    case CDC_SET_LINE_CODING:
-      linecoding.bitrate    = (uint32_t)(pbuf[0] | (pbuf[1] << 8) | \
-                                         (pbuf[2] << 16) | (pbuf[3] << 24));
-      linecoding.format     = pbuf[4];
-      linecoding.paritytype = pbuf[5];
-      linecoding.datatype   = pbuf[6];
+            break;
 
-      /* Add your code here */
-      break;
+        case CDC_SET_CONTROL_LINE_STATE:  // NOLINT(bugprone-branch-clone)
+            break;
 
-    case CDC_GET_LINE_CODING:
-      pbuf[0] = (uint8_t)(linecoding.bitrate);
-      pbuf[1] = (uint8_t)(linecoding.bitrate >> 8);
-      pbuf[2] = (uint8_t)(linecoding.bitrate >> 16);
-      pbuf[3] = (uint8_t)(linecoding.bitrate >> 24);
-      pbuf[4] = linecoding.format;
-      pbuf[5] = linecoding.paritytype;
-      pbuf[6] = linecoding.datatype;
+        case CDC_SEND_BREAK:
+            break;
 
-      /* Add your code here */
-      break;
+        default:
+            break;
+    }
 
-    case CDC_SET_CONTROL_LINE_STATE:
-      /* Add your code here */
-      break;
-
-    case CDC_SEND_BREAK:
-      /* Add your code here */
-      break;
-
-    default:
-      break;
-  }
-
-  return (0);
+    return (0);
 }
 
-static int8_t CDC_Receive(uint8_t *Buf, uint32_t *Len)
-{
-    std::copy(Buf, Buf + *Len, host_comms_control_task::_tasks.second->tx_buf.begin());
-    return USBD_CDC_TransmitPacket(&host_comms_control_task::_tasks.second->usb_handle);
+// NOLINTNEXTLINE(readability-non-const-parameter)
+static auto CDC_Receive(uint8_t *Buf, uint32_t *Len) -> int8_t {
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    std::copy(Buf, Buf + *Len,
+              host_comms_control_task::_tasks.second->tx_buf.begin());
+    return USBD_CDC_TransmitPacket(
+        &host_comms_control_task::_tasks.second->usb_handle);
 }
