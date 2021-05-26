7b4a3d128503714bd9703546db689b4576476fee
Seth Foster <seth@opentrons.com>
2021-05-26 12:52:53 -0400

    fixup: keep irqs on, do an asm
    Need to do an asm branch so we don't mess with the stack

--- stm32-modules/heater-shaker/firmware/system/system_hardware.c
+++ stm32-modules/heater-shaker/firmware/system/system_hardware.c
@@ -14,41 +14,49 @@ void system_hardware_setup(void) {
     HAL_GPIO_Init(SOFTPOWER_PORT, &gpio_init);
 }
 
+// This is the start of the sys memory region from the datasheet. It should be the
+// same for at least all the f303s, including the vdt6 which is on the nffs and the
+// vc that will be on the ff.
+#define SYSMEM_START 0x1fffd800
+#define SYSMEM_BOOT (SYSMEM_START + 4)
+
+// address 4 in the bootable region is the address of the first instruction that
+// should run, aka the data that should be loaded into $pc.
+const uint32_t *const sysmem_boot_loc = (uint32_t*)SYSMEM_BOOT;
+
 void system_hardware_enter_bootloader(void) {
-  // a magic function pointer that we'll bind to the first code address in system
-  // memory
-  void (*sysmem_boot)(void);
-  // This is the start of the sys memory region from the datasheet. It should be the
-  // same for at least all the f303s, including the vdt6 which is on the nffs and the
-  // vc that will be on the ff.
-  volatile uint32_t sysmem_start = 0x1FFFD800;
+
   // We have to uninitialize as many of the peripherals as possible, because the bootloader
-  // expects to start as the system comes up, which means we better not have systick running
-  // (SysTick is a magic global defined by cmsis)
+  // expects to start as the system comes up
+
+  // The HAL has ways to turn off all the core clocking and the clock security system
+  HAL_RCC_DisableCSS();
+  HAL_RCC_DeInit();
+
+  // systick should be off at boot
   SysTick->CTRL = 0;
   SysTick->LOAD = 0;
   SysTick->VAL = 0;
-  // we should get our interrupts disabled (__disable_irq is a magic intrinsic defined by CMSIS)
-  __disable_irq();
-  // The HAL has ways to turn off all the peripheral clocks and the clock security system
-  HAL_RCC_DisableCSS();
-  HAL_RCC_DeInit();
+
   // We have to make sure that the processor is mapping the system memory region to address 0,
   // which the bootloader expects
   __HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
   // and now we're ready to set the system up to start executing system flash.
   // arm cortex initialization means that
-  // - address 4 in the bootable region is the address of the first instruction that
-  //   should run, aka the data that should be loaded into $pc, so we want to make our
-  //   function pointer be
-  sysmem_boot = (void (*)(void))( // a cast to something that looks like a pointer-to-function
-                                  // that takes no arguments and returns no values
-    *(                            // of the value, which should itself be a memory access,
-      (uint32_t*)(sysmem_start + 4))); // 4 bytes past the magic value we stored above
 
-  // - address 0 in the bootable region is the address where the processor should start its stack
-  //   which we have to do as late as possible because as soon as we do this the c and c++ runtime
-  //   environment is no longer valid
-  __set_MSP(*((uint32_t*)sysmem_start));
-  sysmem_boot();
+  // address 0 in the bootable region is the address where the processor should start its stack
+  // which we have to do as late as possible because as soon as we do this the c and c++ runtime
+  // environment is no longer valid
+  __set_MSP(*((uint32_t*)SYSMEM_START));
+
+  // finally, jump to the bootloader. we do this in inline asm because we need
+  // this to be a naked call (no caller-side prep like stacking return addresses)
+  // and to have a naked function you need to define it as a function, not a
+  // function pointer, and we don't statically know the address here since it is
+  // whatever's contained in that second word of the bsystem memory region.
+asm volatile (
+  "bx %0"
+  : // no outputs
+  : "r" (*sysmem_boot_loc)
+  : "memory"  );
 }
