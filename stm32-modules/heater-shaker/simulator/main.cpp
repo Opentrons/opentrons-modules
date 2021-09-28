7f2f2781732275ee892de76a7ea8b476a688c369
Derek Maggio <derek.maggio@opentrons.com>
2021-09-28 08:40:42 -0700

    Add command line parsing

--- stm32-modules/heater-shaker/simulator/main.cpp
+++ stm32-modules/heater-shaker/simulator/main.cpp
@@ -6,24 +6,47 @@
 #include "simulator/motor_thread.hpp"
 #include "simulator/simulator_queue.hpp"
 #include "simulator/system_thread.hpp"
+#include <boost/program_options.hpp>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    auto system = system_thread::build();
-    auto heater = heater_thread::build();
-    auto motor = motor_thread::build();
-    auto comms = comm_thread::build();
-    auto tasks = tasks::Tasks<SimulatorMessageQueue>(heater.task, comms.task,
-                                                     motor.task, system.task);
-    comm_thread::handle_input(tasks);
-    system.handle->request_stop();
-    heater.handle->request_stop();
-    motor.handle->request_stop();
-    comms.handle->request_stop();
-    system.handle->join();
-    heater.handle->join();
-    motor.handle->join();
-    comms.handle->join();
-    return 0;
+    bool use_stdin = false;
+    bool use_socket = false;
+
+    namespace po = boost::program_options;
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "produce help message")
+            ("stdin", po::bool_switch(&use_stdin), "Use stdin to provide G-Codes")
+            ("socket", po::bool_switch(&use_socket), "Use socket to provide G-Codes")
+            ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    bool options_provided = argc > 1;
+    bool both_stdin_and_socket_specified = use_stdin && use_socket;
+
+    if (!options_provided) {
+        cout << endl << "ERROR: You must provide either the --stdin OR the --socket option" << endl << endl;
+        cout << desc << endl;
+        exit(1);
+    }
+
+    if (both_stdin_and_socket_specified) {
+        cout << endl << "ERROR: You may only provide either the --stdin OR the --socket option, not both" << endl << endl;
+        cout << desc << endl;
+        exit(1);
+    }
+
+    if (use_stdin) {
+        cout << "Using stdin" << endl;
+    }
+
+    if (use_socket) {
+        cout << "Using socket" << endl;
+    }
 }
