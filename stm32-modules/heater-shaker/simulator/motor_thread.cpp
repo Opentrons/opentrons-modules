9307861e8ca95a3769bdbae81f03d34930c4375b
pmoegenburg <pbm53@cornell.edu>
2021-09-29 16:46:01 -0400

    initial updates

--- stm32-modules/heater-shaker/simulator/motor_thread.cpp
+++ stm32-modules/heater-shaker/simulator/motor_thread.cpp
@@ -51,14 +51,51 @@ struct SimMotorPolicy {
 
     auto delay_ticks(uint16_t ticks) -> void { static_cast<void>(ticks); }
 
-    auto plate_lock_set_power(float power) -> void { static_cast<void>(power); }
+    auto plate_lock_set_power(float power) -> void {
+        plate_lock_power = power;
+        plate_lock_enabled = true;
+        plate_lock_braked = false;
+    }
 
-    auto plate_lock_disable() -> void {}
+    auto plate_lock_disable() -> void {
+        plate_lock_enabled = false;
+        plate_lock_power = 0.0;
+    }
+
+    auto plate_lock_get_power() const -> float { return plate_lock_power; }
+
+    auto plate_lock_enabled() const -> bool { return plate_lock_enabled; }
+
+    auto plate_lock_brake() -> void {
+        plate_lock_braked = true;
+        plate_lock_power = 0.0;
+    }
+
+    auto plate_lock_braked() const -> bool { return plate_lock_braked; }
+
+    auto plate_lock_open_sensor_read() const -> bool {
+        if (!plate_lock_braked) {
+            return false;
+        } else {
+            return plate_lock_power < 0.0 ? true : false;
+        }
+    }
+
+    auto plate_lock_closed_sensor_read() const -> bool {
+        if (!plate_lock_braked) {
+            return false;
+        } else {
+            return plate_lock_power > 0.0 ? true : false;
+        }
+    }
 
   private:
     int16_t rpm_setpoint = 0;
     int16_t rpm_current = 0;
     int32_t ramp_rate = DEFAULT_RAMP_RATE_RPM_PER_S;
+    float plate_lock_power = 0;
+    bool plate_lock_enabled = false;
+    bool plate_lock_braked = false;
 };
 
 struct motor_thread::TaskControlBlock {
