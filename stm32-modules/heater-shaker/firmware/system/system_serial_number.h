cdfb7258259d4237821fd5edcb8cc2aee7785ec2
pmoegenburg <pbm53@cornell.edu>
2021-09-30 13:51:33 -0400

    initial draft

--- stm32-modules/heater-shaker/firmware/system/system_serial_number.h
+++ stm32-modules/heater-shaker/firmware/system/system_serial_number.h
@@ -8,7 +8,7 @@ extern "C" {
 #include <stdbool.h>
 #include <stddef.h>
 
-bool system_set_serial_number(uint64_t to_write, uint8_t address);
+bool system_set_serial_number(uint64_t to_write1, uint64_t to_write2, uint64_t to_write3);
 
 uint64_t system_get_serial_number(uint8_t address);
 
