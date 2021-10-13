421ab36d274f07dc0603ab82f4b8562157f63858
Derek Maggio <derek.maggio@opentrons.com>
2021-10-13 12:35:14 -0700

    Pull out socket to be an attribute of SocketSimDriver

--- stm32-modules/heater-shaker/include/simulator/socket_sim_driver.hpp
+++ stm32-modules/heater-shaker/include/simulator/socket_sim_driver.hpp
@@ -4,6 +4,7 @@
 #include "heater-shaker/tasks.hpp"
 #include "simulator/sim_driver.hpp"
 #include "simulator/simulator_queue.hpp"
+#include <boost/asio.hpp>
 
 namespace socket_sim_driver {
 
@@ -15,12 +16,15 @@ struct AddressInfo {
 class SocketSimDriver : public sim_driver::SimDriver {
     static const std::string name;
     AddressInfo address_info;
+    std::unique_ptr<boost::asio::ip::tcp::socket> s;
 
   public:
     SocketSimDriver(std::string);
     std::string get_host();
     int get_port();
     const std::string& get_name() const;
+    std::unique_ptr<boost::asio::ip::tcp::socket> get_socket();
+
     void write(std::string message);
     void read(tasks::Tasks<SimulatorMessageQueue>& tasks);
 };
