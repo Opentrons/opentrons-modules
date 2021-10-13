fc3f3c0282e848dfab19a8bc8f4760dc1e6deb6e
Derek Maggio <derek.maggio@opentrons.com>
2021-10-13 08:35:12 -0700

    Remove unused class definitions

--- stm32-modules/heater-shaker/include/simulator/sim_driver.hpp
+++ stm32-modules/heater-shaker/include/simulator/sim_driver.hpp
@@ -12,28 +12,4 @@ class SimDriver {
     virtual void write() = 0;
     virtual void read(tasks::Tasks<SimulatorMessageQueue>& tasks) = 0;
 };
-
-class SocketSimDriver : public SimDriver {
-    static const std::string name;
-    std::string host;
-    int port;
-
-  public:
-    SocketSimDriver(std::string);
-    std::string get_host();
-    int get_port();
-    const std::string& get_name() const;
-    void write();
-    void read(tasks::Tasks<SimulatorMessageQueue>& tasks);
-};
-
-class StdinSimDriver : public SimDriver {
-    static const std::string name;
-
-  public:
-    StdinSimDriver();
-    const std::string& get_name() const;
-    void write();
-    void read(tasks::Tasks<SimulatorMessageQueue>& tasks);
-};
-}  // namespace sim_driver
+}
