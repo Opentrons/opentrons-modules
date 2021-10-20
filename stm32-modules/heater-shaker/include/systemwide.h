cc5c92ff2eb9cee2ac4d10ba873ed9927c029e2a
pmoegenburg <pbm53@cornell.edu>
2021-10-20 15:48:08 -0400

    updated systemwide for use in c++ and c files

---
+++ stm32-modules/heater-shaker/include/systemwide.h
@@ -0,0 +1,11 @@
+// included in c++ and c files
+#pragma once //may work
+
+/* size of transmission buffer for setting led */
+#define TXBUFFERSIZE                    12
+
+/* size of array for setting serial number */
+#define SERIAL_NUMBER_LENGTH            24
+
+
+//get rid of system_hardware.h TXBUFFERSIZE, use this instead
--- stm32-modules/heater-shaker/include/systemwide.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-
-class systemwide {
-  public:
-    static constexpr std::size_t SERIAL_NUMBER_LENGTH = 24;
-
-    static constexpr std::size_t TXBUFFERSIZE = 12;
-};
