81ad2dc7ee35a03dd58aa247707ee35cb9ce17d5
Frank Sinapi <frank.sinapi@opentrons.com>
2021-11-08 15:52:52 -0500

    - Updates to thermocycler github workflow so it isn't just a copy of the heater-shaker - Added a dummy test executable for thermocycler-refresh - Addressed linting issue with a hal-copied header in thermocycler-refresh

--- stm32-modules/thermocycler-refresh/firmware/system/stm32g4xx_hal_conf.h
+++ stm32-modules/thermocycler-refresh/firmware/system/stm32g4xx_hal_conf.h
@@ -146,7 +146,7 @@ extern "C" {
 #if !defined(HSI48_VALUE)
 #define HSI48_VALUE                                                       \
     (48000000UL) /*!< Value of the Internal High Speed oscillator for USB \
-                    FS/RNG in Hz. The real value my vary depending on                                      \
+                    FS/RNG in Hz. The real value my vary depending on     \
                     manufacturing process variations.*/
 #endif           /* HSI48_VALUE */
 
