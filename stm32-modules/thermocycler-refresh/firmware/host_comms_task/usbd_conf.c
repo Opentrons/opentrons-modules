797af2c2e815a9336844b457609cf0edd1a5c995
Frank Sinapi <frank.sinapi@opentrons.com>
2021-11-10 10:29:17 -0500

    Removed some dead code in thermocycler-refresh usbd_conf.c

--- stm32-modules/thermocycler-refresh/firmware/host_comms_task/usbd_conf.c
+++ stm32-modules/thermocycler-refresh/firmware/host_comms_task/usbd_conf.c
@@ -51,19 +51,6 @@ extern void SystemClock_Config(void);
 
 void HAL_PCD_MspInit(PCD_HandleTypeDef *hpcd)
 {
-#if 0
-  GPIO_InitTypeDef GPIO_InitStruct;
-  /* Enable the GPIOA clock for USB DataLines */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /* Configure USB DM and DP pins */
-  GPIO_InitStruct.Pin = (GPIO_PIN_11 | GPIO_PIN_12);
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF14_USB;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-#endif 
   /* Enable USB FS Clock */
   __HAL_RCC_USB_CLK_ENABLE();
 
