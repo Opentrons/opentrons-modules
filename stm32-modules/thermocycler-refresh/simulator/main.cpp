b270824f72033cd04ef9a2549db2cb3e07cd619c
Frank Sinapi <frank.sinapi@opentrons.com>
2021-11-10 10:13:55 -0500

    Formatted code

--- stm32-modules/thermocycler-refresh/simulator/main.cpp
+++ stm32-modules/thermocycler-refresh/simulator/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <memory>
 
-#include "thermocycler-refresh/tasks.hpp"
 #include "simulator/cli_parser.hpp"
 #include "simulator/comm_thread.hpp"
 #include "simulator/sim_driver.hpp"
 #include "simulator/simulator_queue.hpp"
 #include "simulator/system_thread.hpp"
+#include "thermocycler-refresh/tasks.hpp"
 
 using namespace std;
 
-int main(int argc, char *argv[]) { 
+int main(int argc, char *argv[]) {
     auto sim_driver = cli_parser::get_sim_driver(argc, argv);
     auto system = system_thread::build();
     auto comms = comm_thread::build(std::move(sim_driver));
@@ -20,9 +20,9 @@ int main(int argc, char *argv[]) {
 
     system.handle->request_stop();
     comms.handle->request_stop();
-    
+
     system.handle->join();
     comms.handle->join();
-    
-    return 0; 
+
+    return 0;
 }
