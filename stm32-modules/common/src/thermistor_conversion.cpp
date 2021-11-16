7a168c970afb5416edffbcdebaf44fc521f73703
Frank Sinapi <frank.sinapi@opentrons.com>
2021-11-16 10:18:44 -0500

    Added support in thermistor_conversion for using different thermistors

--- stm32-modules/common/src/thermistor_conversion.cpp
+++ stm32-modules/common/src/thermistor_conversion.cpp
@@ -10,10 +10,19 @@ using namespace thermistor_conversion;
 Conversion::Conversion(ThermistorType thermistor,
                        double bias_resistance_nominal_kohm,
                        uint8_t adc_max_bits)
-    : adc_max(static_cast<double>((1U << adc_max_bits) - 1)),
-      adc_max_result(
+    : _adc_max(static_cast<double>((1U << adc_max_bits) - 1)),
+      _adc_max_result(
           static_cast<uint16_t>(static_cast<uint32_t>(1U << adc_max_bits) - 1)),
-      bias_resistance_kohm(bias_resistance_nominal_kohm) {}
+      _bias_resistance_kohm(bias_resistance_nominal_kohm),
+      _type(thermistor) {}
+
+Conversion::Conversion(ThermistorType thermistor, double bias_resistance_nominal_kohm,
+               uint16_t adc_max_value, bool is_signed)
+    : _adc_max(static_cast<double>(adc_max_value)),
+      _adc_max_result(
+          static_cast<uint16_t>(static_cast<uint32_t>(adc_max_value))),
+      _bias_resistance_kohm(bias_resistance_nominal_kohm),
+      _type(thermistor) {}
 
 [[nodiscard]] auto Conversion::convert(uint16_t adc_count) const -> Result {
     auto resistance = resistance_from_adc(adc_count);
@@ -25,33 +34,36 @@ Conversion::Conversion(ThermistorType thermistor,
 
 [[nodiscard]] auto Conversion::resistance_from_adc(uint16_t adc_count) const
     -> Result {
-    if (adc_count == adc_max_result) {
+    if (adc_count == _adc_max_result) {
         return Result(Error::OUT_OF_RANGE_LOW);
     }
     if (adc_count == 0) {
         return Result(Error::OUT_OF_RANGE_HIGH);
     }
-    return Result(bias_resistance_kohm /
-                  ((adc_max / static_cast<double>(adc_count)) - 1.0));
+    return Result(_bias_resistance_kohm /
+                  ((_adc_max / static_cast<double>(adc_count)) - 1.0));
 }
 
 [[nodiscard]] auto Conversion::temperature_from_resistance(
     double resistance) const -> Result {
-    auto first_less = std::find_if(
-        lookups::NTCG104ED104DTDSX().cbegin(),
-        lookups::NTCG104ED104DTDSX().end(),
-        [resistance](auto elem) { return elem.first < resistance; });
-    if (first_less == lookups::NTCG104ED104DTDSX().cend()) {
-        return Result(Error::OUT_OF_RANGE_HIGH);
+    auto entries = resistance_table_lookup(resistance);
+    if(std::holds_alternative<TableError>(entries))
+    {
+        if(std::get<TableError>(entries) == TableError::TABLE_END)
+        {
+            return Result(Error::OUT_OF_RANGE_HIGH);
+        }
+        else
+        {
+            return Result(Error::OUT_OF_RANGE_LOW);
+        }
     }
-    if (first_less == lookups::NTCG104ED104DTDSX().cbegin()) {
-        return Result(Error::OUT_OF_RANGE_LOW);
-    }
-    auto after_temp = static_cast<double>(first_less->second);
-    auto after_res = first_less->first;
-    auto before = first_less - 1;
-    auto before_temp = static_cast<double>(before->second);
-    auto before_res = before->first;
+    auto entry_pair = std::get<TableEntryPair>(entries);
+
+    auto after_temp = static_cast<double>(entry_pair.first.second);
+    auto after_res = entry_pair.first.first;
+    auto before_temp = static_cast<double>(entry_pair.second.second);
+    auto before_res = entry_pair.second.first;
 
     return Result((after_temp - before_temp) / (after_res - before_res) *
                       (resistance - before_res) +
@@ -60,27 +72,109 @@ Conversion::Conversion(ThermistorType thermistor,
 
 [[nodiscard]] auto Conversion::backconvert(double temperature) const
     -> uint16_t {
-    auto first_greater = std::find_if(
-        lookups::NTCG104ED104DTDSX().cbegin(),
-        lookups::NTCG104ED104DTDSX().end(),
-        [temperature](auto elem) { return elem.second > temperature; });
-
-    if (first_greater == lookups::NTCG104ED104DTDSX().end()) {
-        return adc_max_result;
+    auto entries = temperature_table_lookup(temperature);
+    if(std::holds_alternative<TableError>(entries))
+    {
+        if(std::get<TableError>(entries) == TableError::TABLE_END)
+        {
+            return _adc_max_result;
+        }
+        else
+        {
+            return 0;
+        }
     }
-    if (first_greater == lookups::NTCG104ED104DTDSX().cbegin()) {
-        return 0;
-    }
+    auto entry_pair = std::get<TableEntryPair>(entries);
 
-    auto after_temp = static_cast<double>(first_greater->second);
-    auto after_res = static_cast<double>(first_greater->first);
-    auto before = first_greater - 1;
-    auto before_temp = static_cast<double>(before->second);
-    auto before_res = static_cast<double>(before->first);
+    auto after_temp = static_cast<double>(entry_pair.first.second);
+    auto after_res = entry_pair.first.first;
+    auto before_temp = static_cast<double>(entry_pair.second.second);
+    auto before_res = entry_pair.second.first;
     double resistance =
         ((after_res - before_res) / (after_temp - before_temp)) *
             (temperature - before_temp) +
         before_res;
-    return static_cast<uint16_t>(adc_max /
-                                 ((bias_resistance_kohm / resistance) + 1.0));
+    return static_cast<uint16_t>(_adc_max /
+                                 ((_bias_resistance_kohm / resistance) + 1.0));
+}
+
+
+[[nodiscard]] auto Conversion::resistance_table_lookup(double resistance) const -> TableResult {
+    auto compare = [resistance](auto elem) { return elem.second < resistance; };
+
+    switch(_type)
+    {
+    case ThermistorType::NTCG104ED104DTDSX: {
+        auto first_less = std::find_if(
+            lookups::NTCG104ED104DTDSX().cbegin(),
+            lookups::NTCG104ED104DTDSX().end(),
+            compare);
+        if(first_less == lookups::NTCG104ED104DTDSX().cbegin())
+        {
+            return TableResult(TableError::TABLE_CBEGIN);
+        }
+        if(first_less == lookups::NTCG104ED104DTDSX().end())
+        {
+            return TableResult(TableError::TABLE_END);
+        }
+        return TableResult(TableEntryPair(*first_less, *(first_less - 1)));
+    }
+        break;
+    case ThermistorType::KS103J2G: {
+        auto first_less = std::find_if(
+            lookups::KS103J2G().cbegin(),
+            lookups::KS103J2G().end(),
+            compare);
+        if(first_less == lookups::KS103J2G().cbegin())
+        {
+            return TableResult(TableError::TABLE_CBEGIN);
+        }
+        if(first_less == lookups::KS103J2G().end())
+        {
+            return TableResult(TableError::TABLE_END);
+        }
+        return TableResult(TableEntryPair(*first_less, *(first_less - 1)));
+    }
+        break;
+    }
+}
+
+[[nodiscard]] auto Conversion::temperature_table_lookup(double temperature) const -> TableResult {
+    auto compare = [temperature](auto elem) { return elem.second > temperature; };
+
+    switch(_type)
+    {
+    case ThermistorType::NTCG104ED104DTDSX: {
+        auto first_less = std::find_if(
+            lookups::NTCG104ED104DTDSX().cbegin(),
+            lookups::NTCG104ED104DTDSX().end(),
+            compare);
+        if(first_less == lookups::NTCG104ED104DTDSX().cbegin())
+        {
+            return TableResult(TableError::TABLE_CBEGIN);
+        }
+        if(first_less == lookups::NTCG104ED104DTDSX().end())
+        {
+            return TableResult(TableError::TABLE_END);
+        }
+        return TableResult(TableEntryPair(*first_less, *(first_less - 1)));
+    }
+        break;
+    case ThermistorType::KS103J2G: {
+        auto first_less = std::find_if(
+            lookups::KS103J2G().cbegin(),
+            lookups::KS103J2G().end(),
+            compare);
+        if(first_less == lookups::KS103J2G().cbegin())
+        {
+            return TableResult(TableError::TABLE_CBEGIN);
+        }
+        if(first_less == lookups::KS103J2G().end())
+        {
+            return TableResult(TableError::TABLE_END);
+        }
+        return TableResult(TableEntryPair(*first_less, *(first_less - 1)));
+    }
+        break;
+    }
 }
--- stm32-modules/include/common/core/thermistor_conversion.hpp
+++ stm32-modules/include/common/core/thermistor_conversion.hpp
@@ -18,29 +18,56 @@
 namespace thermistor_conversion {
 enum class ThermistorType {
     NTCG104ED104DTDSX,
+    KS103J2G
 };
 
 enum class Error { OUT_OF_RANGE_LOW, OUT_OF_RANGE_HIGH };
 
+enum class TableError { TABLE_END, TABLE_CBEGIN };
+
 struct Conversion {
     using Result = std::variant<double, Error>;
-    /*
-    ** Build a converter. The resistance should be in kiloohms to match the
-    *tbales.
-    */
+    // First is resistance, second is temperature
+    using TableEntry = std::pair<double, int16_t>;
+    /** First is After, second is Before (after - 1) */
+    using TableEntryPair = std::pair<TableEntry, TableEntry>;
+    using TableResult = std::variant<TableEntryPair, TableError>;
+    /**
+     * Build a converter. The resistance should be in kiloohms to match the
+     * tables.
+     */
     Conversion(ThermistorType thermistor, double bias_resistance_nominal_kohm,
                uint8_t adc_max_bits);
+    /** 
+     * This initializer builds a converter with a literal bitmap of the
+     * max ADC values instead of the number of bits - required when the
+     * max voltage of the circuit doesn't match with the ref voltage.
+     * 
+     * NOTE - the param is_signed is ignored for now, but is useful to 
+     * force differentiation between constructors.
+     */
+    Conversion(ThermistorType thermistor, double bias_resistance_nominal_kohm,
+               uint16_t adc_max_value, bool is_signed);
     Conversion() = delete;
 
     [[nodiscard]] auto convert(uint16_t adc_reading) const -> Result;
     [[nodiscard]] auto backconvert(double temperature) const -> uint16_t;
 
   private:
-    const double adc_max;
-    const uint16_t adc_max_result;
-    const double bias_resistance_kohm;
+    const double _adc_max;
+    const uint16_t _adc_max_result;
+    const double _bias_resistance_kohm;
+    const ThermistorType _type;
     [[nodiscard]] auto resistance_from_adc(uint16_t adc_count) const -> Result;
     [[nodiscard]] auto temperature_from_resistance(double resistance) const
         -> Result;
+    /**
+     * Looks for the first table entry with a resistance GREATER than the input
+     */
+    [[nodiscard]] auto resistance_table_lookup(double resistance) const -> TableResult;
+    /**
+     * Looks for the first table entry with a temperature LESS THAN than the input
+     */
+    [[nodiscard]] auto temperature_table_lookup(double temperature) const -> TableResult;
 };
 };  // namespace thermistor_conversion
