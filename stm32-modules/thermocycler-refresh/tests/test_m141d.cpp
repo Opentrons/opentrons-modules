82ff18e39a40180ffc8df9b25cb51234c5744102
Frank Sinapi <frank.sinapi@opentrons.com>
2021-11-22 09:38:32 -0500

    Added unit tests for 105.D and 141.D command parsing

---
+++ stm32-modules/thermocycler-refresh/tests/test_m141d.cpp
@@ -0,0 +1,38 @@
+#include <array>
+
+#include "catch2/catch.hpp"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+#include "thermocycler-refresh/gcodes.hpp"
+#pragma GCC diagnostic pop
+
+SCENARIO("GetLidTemperatureDebug (M141.D) parser works",
+         "[gcode][parse][M141.d]") {
+    GIVEN("a response buffer large enough for the formatted response") {
+        std::string buffer(256, 'c');
+        WHEN("filling response") {
+            auto written = gcode::GetLidTemperatureDebug::write_response_into(
+                buffer.begin(), buffer.end(), 10.0, 40);
+            THEN("the response should be written in full") {
+                REQUIRE_THAT(buffer,
+                             Catch::Matchers::StartsWith(
+                                "M141.D LT:10.00 LA:40 OK\n"));
+                REQUIRE(written != buffer.begin());
+            }
+        }
+    }
+
+    GIVEN("a response buffer not large enough for the formatted response") {
+        std::string buffer(16, 'c');
+        WHEN("filling response") {
+            auto written = gcode::GetLidTemperatureDebug::write_response_into(
+                buffer.begin(), buffer.begin() + 7, 10.0, 40);
+            THEN("the response should write only up to the available space") {
+                std::string response = "M141.Dcccccccccc";
+                response.at(6) = '\0';
+                REQUIRE_THAT(buffer, Catch::Matchers::Equals(response));
+                REQUIRE(written != buffer.begin());
+            }
+        }
+    }
+}
