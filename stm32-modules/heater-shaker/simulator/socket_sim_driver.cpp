306e280e8f5a71fa968d08bbf7642c2272a37aa6
Derek Maggio <derek.maggio@opentrons.com>
2021-11-22 13:11:56 -0800

    feat: Add host name resolving to heater-shaker and thermocycler

--- stm32-modules/heater-shaker/simulator/socket_sim_driver.cpp
+++ stm32-modules/heater-shaker/simulator/socket_sim_driver.cpp
@@ -16,10 +16,13 @@ const std::string SOCKET_DRIVER_NAME = "Socket";
 std::unique_ptr<boost::asio::ip::tcp::socket> connect_to_socket(
     std::string host, int port) {
     boost::asio::io_service io_context;
+    boost::asio::ip::tcp::resolver resolver(io_context);
+    auto endpoints  = resolver.resolve(host, std::to_string(port));
+    std::string parsed_host = endpoints.begin()->endpoint().address().to_string();
 
     auto socket = std::make_unique<boost::asio::ip::tcp::socket>(io_context);
     boost::asio::ip::tcp::endpoint endpoint(
-        boost::asio::ip::address::from_string(host), port);
+        boost::asio::ip::address::from_string(parsed_host), port);
     boost::system::error_code ec;
     socket->connect(endpoint, ec);
     if (ec) {
