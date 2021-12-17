1627ef3d7974e9f9932151ecaef783765533ffe0
Frank Sinapi <frank.sinapi@opentrons.com>
2021-12-17 15:17:52 -0500

    Clang formatting... and changed the motor spi C code to unlinted

--- stm32-modules/include/thermocycler-refresh/firmware/motor_spi_policy.hpp
+++ stm32-modules/include/thermocycler-refresh/firmware/motor_spi_policy.hpp
@@ -6,11 +6,11 @@
 class MotorSpiPolicy {
   public:
     using ReadRT = std::optional<tmc2130::RegisterSerializedType>;
-    auto transmit_receive(tmc2130::MessageT& data) 
+    auto transmit_receive(tmc2130::MessageT& data)
         -> std::optional<tmc2130::MessageT> {
         using RT = std::optional<tmc2130::MessageT>;
         tmc2130::MessageT retBuf = {0};
-        if(motor_spi_sendreceive(data.data(), retBuf.data(), data.size())) {
+        if (motor_spi_sendreceive(data.data(), retBuf.data(), data.size())) {
             return RT(retBuf);
         }
         return RT(0);
