c3c0ebd30a47bc57a3b086fd51a177bfad7f3a5f
Frank Sinapi <frank.sinapi@opentrons.com>
2022-03-17 11:20:13 -0400

    Added M18 to deactivate all thermal systems

--- stm32-modules/include/thermocycler-refresh/thermocycler-refresh/gcodes.hpp
+++ stm32-modules/include/thermocycler-refresh/thermocycler-refresh/gcodes.hpp
@@ -1440,4 +1440,29 @@ struct CloseLid {
     }
 };
 
+struct DeactivateAll {
+    using ParseResult = std::optional<DeactivateAll>;
+    static constexpr auto prefix = std::array{'M', '1', '8'};
+    static constexpr const char* response = "M18 OK\n";
+
+    template <typename InputIt, typename Limit>
+    requires std::forward_iterator<InputIt> &&
+        std::sized_sentinel_for<Limit, InputIt>
+    static auto parse(const InputIt& input, Limit limit)
+        -> std::pair<ParseResult, InputIt> {
+        auto working = prefix_matches(input, limit, prefix);
+        if (working == input) {
+            return std::make_pair(ParseResult(), input);
+        }
+        return std::make_pair(ParseResult(DeactivateAll()), working);
+    }
+
+    template <typename InputIt, typename InLimit>
+    requires std::forward_iterator<InputIt> &&
+        std::sized_sentinel_for<InputIt, InLimit>
+    static auto write_response_into(InputIt buf, InLimit limit) -> InputIt {
+        return write_string_to_iterpair(buf, limit, response);
+    }
+};
+
 }  // namespace gcode
