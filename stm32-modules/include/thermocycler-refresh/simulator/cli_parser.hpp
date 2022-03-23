015cab162221b609ec2a6b8d09d578bc7d774d80
Frank Sinapi <frank.sinapi@opentrons.com>
2022-03-23 10:56:48 -0400

    feat(thermocycler-refresh): add simulated temperature (#271)
    * feat(thermocycler-refresh): add periodic temperature data to simulator
    
    * Added a clarifying comment on a pair type
    
    * Cleaned up lambda syntax
    
    * Made periodic data update even if power isn't changed
    * added some better commenting in periodic_data_thread
    
    * sharing is caring

--- stm32-modules/include/thermocycler-refresh/simulator/cli_parser.hpp
+++ stm32-modules/include/thermocycler-refresh/simulator/cli_parser.hpp
@@ -4,5 +4,16 @@
 #include "simulator/sim_driver.hpp"
 
 namespace cli_parser {
-std::shared_ptr<sim_driver::SimDriver> get_sim_driver(int, char**);
-}
+/**
+ * First value is the sim input driver, second input is a boolean
+ * set to true if the sim should run in realtime and false if it
+ * should run in simulated time
+ */
+using RT = std::pair<std::shared_ptr<sim_driver::SimDriver>, bool>;
+
+/**
+ * Parse the inputs and determine 1) what kind of input should be
+ * used 2) whether the simulation should be realtime or accelerated
+ */
+RT get_sim_driver(int, char**);
+}  // namespace cli_parser
