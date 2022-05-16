9fbbb9a9cd7c5371ec52b7041485eeaf4632b591
Frank Sinapi <frank.sinapi@opentrons.com>
2022-05-16 17:26:21 -0400

    chore(thermocycler-gen2): set up channel testing script (#303)
    * Added a "channel test" script to get temperature data for each temperature while driving to target temperatures
    
    * Fixed an argparse help line
    
    * Added a couple print statements, and edits per Amit's review:
    - Single quote --> Double quote docstrings
    - Take advantage of `@dataclass` decorator

--- stm32-modules/thermocycler-gen2/scripts/plot_temp.py
+++ stm32-modules/thermocycler-gen2/scripts/plot_temp.py
@@ -18,19 +18,23 @@ def graphTemperatures(ser_in : serial.Serial = None, _callback = None):
     SAMPLE_LIMIT = 500
     SAMPLE_INTERVAL = 100
     x_time, y_lid , y_left, y_center, y_right, y_sink= [], [], [], [], [], []
+    lp, cp, rp = [], [], []
     if not ser_in:
         print('Building new serial')
         ser = test_utils.build_serial()
     else:
         ser = ser_in
 
-    fig = pp.figure()
-    lid_line, = pp.plot_date(x_time, y_lid, '-b', label='Lid (C)')
+    fig, axs = pp.subplots(2)
+    lid_line, = axs[0].plot_date(x_time, y_lid, '-b', label='Lid (C)')
     #plate_line, = pp.plot_date(x_time, y_plate, '-r', label='Plate (C)')
-    left_line, = pp.plot_date(x_time, y_left, '-r', label='Left (C)')
-    right_line, = pp.plot_date(x_time, y_right, '-c', label='Right (C)')
-    center_line, = pp.plot_date(x_time, y_center, '-y', label='Center (C)')
-    sink_line, = pp.plot_date(x_time, y_sink, '-g', label='Heatsink (C)')
+    left_line, = axs[0].plot_date(x_time, y_left, '-r', label='Left (C)')
+    right_line, = axs[0].plot_date(x_time, y_right, '-c', label='Right (C)')
+    center_line, = axs[0].plot_date(x_time, y_center, '-y', label='Center (C)')
+    sink_line, = axs[0].plot_date(x_time, y_sink, '-g', label='Heatsink (C)')
+    lp_line, = axs[1].plot_date(x_time, lp, '-r', label='left power')
+    cp_line, = axs[1].plot_date(x_time, cp, '-c', label='center power')
+    rp_line, = axs[1].plot_date(x_time, rp, '-y', label='right power')
     fig.legend()
 
     # Sub function for the updating
@@ -44,6 +48,10 @@ def graphTemperatures(ser_in : serial.Serial = None, _callback = None):
         y_left.append(left)
         y_right.append(right)
         y_center.append(center)
+        l, c, r = test_utils.get_thermal_power(ser)
+        lp.append(l)
+        cp.append(c)
+        rp.append(r)
 
         if _callback:
             _callback(lid, hs, right, left, center)
@@ -56,6 +64,9 @@ def graphTemperatures(ser_in : serial.Serial = None, _callback = None):
             y_left.pop(0)
             y_right.pop(0)
             y_center.pop(0)
+            lp.pop(0)
+            cp.pop(0)
+            rp.pop(0)
             
         lid_line.set_data(x_time, y_lid)
         #plate_line.set_data(x_time, y_plate)
@@ -63,12 +74,19 @@ def graphTemperatures(ser_in : serial.Serial = None, _callback = None):
         right_line.set_data(x_time, y_right)
         center_line.set_data(x_time, y_center)
         sink_line.set_data(x_time, y_sink)
+        lp_line.set_data(x_time, lp)
+        cp_line.set_data(x_time, cp)
+        rp_line.set_data(x_time, rp)
         if(len(x_time) == SAMPLE_LIMIT):
-            fig.gca().set_xlim(left=x_time[0], right = x_time[-1])
+            axs[0].set_xlim(left=x_time[0], right = x_time[-1])
+            axs[1].set_xlim(left=x_time[0], right = x_time[-1])
     
     max_x = datetime.now() + timedelta(milliseconds=SAMPLE_INTERVAL * SAMPLE_LIMIT)
-    fig.gca().set_xlim(left=datetime.now(), right = max_x)
-    fig.gca().set_ylim(bottom=0, top=100)
+    axs[0].set_xlim(left=datetime.now(), right = max_x)
+    axs[1].set_xlim(left=datetime.now(), right = max_x)
+    
+    axs[0].set_ylim(bottom=0, top=100)
+    axs[1].set_ylim(bottom=-1.2, top=1.2)
     animation = FuncAnimation(fig, update, interval=SAMPLE_INTERVAL)
     pp.show()
 
