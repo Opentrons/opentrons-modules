c40d6118e9f08e10227d2cf33d1b72b282c81c63
Frank Sinapi <frank.sinapi@opentrons.com>
2022-07-11 16:55:51 -0400

    Added M105.D GetTemperatureDebug to get ADC info

--- stm32-modules/tempdeck-gen3/tests/test_host_comms_task.cpp
+++ stm32-modules/tempdeck-gen3/tests/test_host_comms_task.cpp
@@ -160,6 +160,64 @@ SCENARIO("host comms commands to system task") {
     }
 }
 
+SCENARIO("host comms commands to thermal task") {
+    auto *tasks = tasks::BuildTasks();
+    std::string tx_buf(128, 'c');
+    WHEN("sending gcode M105.D") {
+        auto message_text = std::string("M105.D\n");
+        auto message_obj =
+            messages::HostCommsMessage(messages::IncomingMessageFromHost(
+                &*message_text.begin(), &*message_text.end()));
+        REQUIRE(tasks->_comms_queue.try_send(message_obj));
+        auto written =
+            tasks->_comms_task.run_once(tx_buf.begin(), tx_buf.end());
+        THEN("the task does not immediately ack") {
+            REQUIRE(written == tx_buf.begin());
+        }
+        THEN("a message is sent to the thermal task") {
+            REQUIRE(tasks->_thermal_queue.has_message());
+            auto thermal_msg = tasks->_thermal_queue.backing_deque.front();
+            REQUIRE(std::holds_alternative<messages::GetTempDebugMessage>(
+                thermal_msg));
+            auto id = std::get<messages::GetTempDebugMessage>(thermal_msg).id;
+            AND_WHEN("sending response with wrong id") {
+                auto response =
+                    messages::GetTempDebugResponse{.responding_to_id = id + 1,
+                                                   .plate_temp = 1.0,
+                                                   .heatsink_temp = 2.0,
+                                                   .plate_adc = 123,
+                                                   .heatsink_adc = 456};
+                tasks->_comms_queue.backing_deque.push_back(response);
+                written =
+                    tasks->_comms_task.run_once(tx_buf.begin(), tx_buf.end());
+                THEN("an error is printed") {
+                    auto expected = errorstring(
+                        errors::ErrorCode::BAD_MESSAGE_ACKNOWLEDGEMENT);
+                    REQUIRE(written == (tx_buf.begin() + strlen(expected)));
+                    REQUIRE_THAT(tx_buf, Catch::Matchers::StartsWith(expected));
+                }
+            }
+            AND_WHEN("sending a good response") {
+                auto response =
+                    messages::GetTempDebugResponse{.responding_to_id = id,
+                                                   .plate_temp = 1.0,
+                                                   .heatsink_temp = 2.0,
+                                                   .plate_adc = 123,
+                                                   .heatsink_adc = 456};
+                tasks->_comms_queue.backing_deque.push_back(response);
+                written =
+                    tasks->_comms_task.run_once(tx_buf.begin(), tx_buf.end());
+                THEN("an error is printed") {
+                    auto expected =
+                        "M105.D PT:1.00 HST:2.00 PA:123 HSA:456 OK\n";
+                    REQUIRE(written == (tx_buf.begin() + strlen(expected)));
+                    REQUIRE_THAT(tx_buf, Catch::Matchers::StartsWith(expected));
+                }
+            }
+        }
+    }
+}
+
 SCENARIO("host comms usb disconnect") {
     auto *tasks = tasks::BuildTasks();
     std::string tx_buf(128, 'c');
---
+++ stm32-modules/tempdeck-gen3/tests/test_m105d.cpp
@@ -0,0 +1,38 @@
+#include <array>
+
+#include "catch2/catch.hpp"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
+#include "tempdeck-gen3/gcodes.hpp"
+#pragma GCC diagnostic pop
+
+SCENARIO("GetTemperatureDebug (M105.D) parser works",
+         "[gcode][parse][m105.d]") {
+    GIVEN("a response buffer large enough for the formatted response") {
+        std::string buffer(256, 'c');
+        WHEN("filling response") {
+            auto written = gcode::GetTemperatureDebug::write_response_into(
+                buffer.begin(), buffer.end(), 10.0, 15.0, 10, 15);
+            THEN("the response should be written in full") {
+                REQUIRE_THAT(
+                    buffer, Catch::Matchers::StartsWith(
+                                "M105.D PT:10.00 HST:15.00 PA:10 HSA:15 OK\n"));
+                REQUIRE(written != buffer.begin());
+            }
+        }
+    }
+
+    GIVEN("a response buffer not large enough for the formatted response") {
+        std::string buffer(16, 'c');
+        WHEN("filling response") {
+            auto written = gcode::GetTemperatureDebug::write_response_into(
+                buffer.begin(), buffer.begin() + 7, 10.0, 15.0, 10, 15);
+            THEN("the response should write only up to the available space") {
+                std::string response = "M105.Dcccccccccc";
+                response.at(6) = '\0';
+                REQUIRE_THAT(buffer, Catch::Matchers::Equals(response));
+                REQUIRE(written != buffer.begin());
+            }
+        }
+    }
+}
