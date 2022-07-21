c4f5dbc2a5b2fc3c64ba56009baa0c35504e6232
Frank Sinapi <frank.sinapi@opentrons.com>
2022-07-21 11:38:20 -0400

    Added M18 to deactivate thermal system

--- stm32-modules/include/tempdeck-gen3/tempdeck-gen3/gcodes.hpp
+++ stm32-modules/include/tempdeck-gen3/tempdeck-gen3/gcodes.hpp
@@ -165,6 +165,37 @@ struct EnterBootloader {
     }
 };
 
+/**
+ * @brief Command to turn off the thermal system. No parameters.
+ *
+ * M18\n
+ *
+ */
+struct DeactivateAll {
+    using ParseResult = std::optional<DeactivateAll>;
+    static constexpr auto prefix = std::array{'M', '1', '8'};
+    static constexpr const char* response = "M18 OK\n";
+
+    template <typename InputIt, typename InLimit>
+    requires std::forward_iterator<InputIt> &&
+        std::sized_sentinel_for<InputIt, InLimit>
+    static auto write_response_into(InputIt buf, InLimit limit) -> InputIt {
+        return write_string_to_iterpair(buf, limit, response);
+    }
+
+    template <typename InputIt, typename Limit>
+    requires std::forward_iterator<InputIt> &&
+        std::sized_sentinel_for<Limit, InputIt>
+    static auto parse(const InputIt& input, Limit limit)
+        -> std::pair<ParseResult, InputIt> {
+        auto working = prefix_matches(input, limit, prefix);
+        if (working == input) {
+            return std::make_pair(ParseResult(), input);
+        }
+        return std::make_pair(ParseResult(DeactivateAll()), working);
+    }
+};
+
 struct GetTemperatureDebug {
     using ParseResult = std::optional<GetTemperatureDebug>;
     static constexpr auto prefix = std::array{'M', '1', '0', '5', '.', 'D'};
