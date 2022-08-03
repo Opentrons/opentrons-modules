71f9f176bfc68f4275ffadaca9b6056696fefac0
pmoegenburg <pbm53@cornell.edu>
2022-08-03 15:48:40 -0400

    added condition

--- stm32-modules/heater-shaker/firmware/system/FreeRTOSConfig.h
+++ stm32-modules/heater-shaker/firmware/system/FreeRTOSConfig.h
@@ -154,12 +154,19 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
+#ifdef ASSERTIONS_ENABLED
+
 #define configASSERT(x)           \
     if ((x) == 0) {               \
         taskDISABLE_INTERRUPTS(); \
         for (;;)                  \
             ;                     \
     }
+#else
+
+#define configASSERT(x) (void)(x)
+
+#endif
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
    standard names. */
