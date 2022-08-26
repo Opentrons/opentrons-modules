2505fb619d66eb7a540777107d0609f98ab54367
Frank Sinapi <frank.sinapi@opentrons.com>
2022-08-26 09:32:57 -0400

    feat(thermocycler-gen2): overshoot/undershoot tuning (#397)
    * Updated overshoot/undershoot calculation scaling and minimum difference to trigger it
    * Do not activate overshoot/undershoot if the target temperature is <5º away, if the hold time is >2 minutes, or if the target is lower than room temp
    * Increased default KP to favor somewhat more aggressive transients
    * Increased effective proportional band by 2x to reduce weird ramps when approaching target temperature
    * Start counting hold time when within 2 degrees of target
    * Move on from overshoot phase as soon as the temperature is within 2 degrees of the overshoot temp
    * Updated `test_pcr_cycle` to use the thermocycler's hold time tracking
    * Added extra overshoot to middle channel
    - Don't move on from overshoot step until every channel is at the overshoot temp
    - Updated I term to 0.05

--- stm32-modules/thermocycler-gen2/scripts/test_pcr_cycle.py
+++ stm32-modules/thermocycler-gen2/scripts/test_pcr_cycle.py
@@ -8,16 +8,17 @@ import sys
 import argparse
 
 def cycle(ser : serial.Serial, volume: float):
-    # Cycle to 95 then 5 and finally back
     # Format is target temp, then hold time
-    targets_pcr = [
-        (95.0, 40.0),
-        (5.0, 50.0),
-        (20.0, 20.0) ]
     targets = [
-        (4.0, 35.0),
-        (15.0, 35.0),
-        (45.0, 35.0),
+        (94.0, 10.0),
+        (70.0, 30.0),
+        (72.0, 30.0) ]
+    targets_old = [
+        (4.0, 10.0),
+        (15.0, 40.0),
+        (94.0, 10.0),
+        (70.0, 35.0),
+        (72.0, 35.0),
         (90.0, 35.0),
         (95.0, 35.0),]
     target_idx = 0
@@ -37,8 +38,8 @@ def cycle(ser : serial.Serial, volume: float):
                     hold_timer = time.time()
                     print('Reached target. Holding')
             else:
-                to_hold = targets[target_idx][1]
-                if time.time() - hold_timer >= to_hold:
+                to_hold = test_utils.get_remaining_hold(ser)
+                if to_hold < 0.1:
                     hold_timer = 0.0
                     target_idx = target_idx + 1
                     if target_idx == len(targets):
@@ -64,6 +65,7 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--constants', type=float, required=False, nargs=3, 
                         metavar=('P','I','D'), 
                         help='define P, I, and D constants for control')
+    parser.add_argument('-v', '--volume', type=float, required=False, default=25, help='volume in µL')
     args = parser.parse_args()
     if args.socket:
         print(f"Opening socket at localhost:{args.socket}")
@@ -77,6 +79,6 @@ if __name__ == '__main__':
         test_utils.set_peltier_pid(args.constants[0], args.constants[1], args.constants[2], ser)
 
     test_utils.set_lid_temperature(105, ser)
-    cycle(ser, 0.0)
+    cycle(ser, args.volume)
     test_utils.deactivate_lid(ser)
     test_utils.deactivate_plate(ser)
