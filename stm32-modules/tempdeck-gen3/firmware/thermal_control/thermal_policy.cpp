89ddd06295860173081b260e12e539f3ee53515a
Frank Sinapi <frank.sinapi@opentrons.com>
2022-08-30 09:46:49 -0400

    Added write protection

--- stm32-modules/tempdeck-gen3/firmware/thermal_control/thermal_policy.cpp
+++ stm32-modules/tempdeck-gen3/firmware/thermal_control/thermal_policy.cpp
@@ -29,7 +29,9 @@ auto ThermalPolicy::set_fan_power(double power) -> bool {
     return thermal_hardware_set_fan_power(power);
 }
 
-auto ThermalPolicy::set_write_protect(bool set) -> void { return; }
+auto ThermalPolicy::set_write_protect(bool set) -> void {
+    thermal_hardware_set_eeprom_write_protect(set);
+}
 
 // NOLINTNEXTLINE(readability-convert-member-functions-to-static)
 auto ThermalPolicy::i2c_write(uint8_t addr, uint8_t data) -> bool {
