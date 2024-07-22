94a1246c60cd8fdd56d85b3d96946785ae728779
Alise Au <20424172+ahiuchingau@users.noreply.github.com>
2024-07-22 15:58:41 -0400

    chore(stacker): create project (#450)
    Project skeleton for the FLEX stacker

---
+++ stm32-modules/flex-stacker/src/errors.cpp
@@ -0,0 +1,30 @@
+#include "flex-stacker/errors.hpp"
+
+using namespace errors;
+
+const char* const NO_ERROR = "";
+const char* const USB_TX_OVERRUN = "ERR001:tx buffer overrun\n";
+const char* const INTERNAL_QUEUE_FULL = "ERR002:internal queue full\n";
+const char* const UNHANDLED_GCODE = "ERR003:unhandled gcode\n";
+const char* const GCODE_CACHE_FULL = "ERR004:gcode cache full\n";
+const char* const BAD_MESSAGE_ACKNOWLEDGEMENT =
+    "ERR005:bad message acknowledgement\n";
+
+const char* const UNKNOWN_ERROR = "ERR-1:unknown error code\n";
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define HANDLE_CASE(errname) \
+    case ErrorCode::errname: \
+        return errname
+
+auto errors::errorstring(ErrorCode code) -> const char* {
+    switch (code) {
+        HANDLE_CASE(NO_ERROR);
+        HANDLE_CASE(USB_TX_OVERRUN);
+        HANDLE_CASE(INTERNAL_QUEUE_FULL);
+        HANDLE_CASE(UNHANDLED_GCODE);
+        HANDLE_CASE(GCODE_CACHE_FULL);
+        HANDLE_CASE(BAD_MESSAGE_ACKNOWLEDGEMENT);
+    }
+    return UNKNOWN_ERROR;
+}
