98d8fc046bc37e3d4fa6dbc7ad7233f3d6e77d38
vegano1 <brayan.almonte@gmail.com>
2024-11-03 16:36:17 -0500

    fix(flex-stacker): clear ack message cache when a stall is detected.

--- stm32-modules/include/flex-stacker/flex-stacker/host_comms_task.hpp
+++ stm32-modules/include/flex-stacker/flex-stacker/host_comms_task.hpp
@@ -215,6 +215,10 @@ class HostCommsTask {
         std::sized_sentinel_for<InputLimit, InputIt>
     auto visit_message(const messages::ErrorMessage& msg, InputIt tx_into,
                        InputLimit tx_limit) -> InputIt {
+        // stall detected, clear the message cache.
+        if (msg.code == errors::ErrorCode::MOTOR_STALL_DETECTED) {
+            ack_only_cache.clear();
+        }
         return errors::write_into_async(tx_into, tx_limit, msg.code);
     }
 
