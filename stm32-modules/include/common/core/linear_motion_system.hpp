a3cdc8a3cd386e7eed8be6f46f0d13fc0d415135
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2024-11-07 00:24:16 -0500

    update MotorState

--- stm32-modules/include/common/core/linear_motion_system.hpp
+++ stm32-modules/include/common/core/linear_motion_system.hpp
@@ -13,20 +13,21 @@ class BeltConfig {
 };
 
 class LeadScrewConfig {
-    public:
-    static constexpr auto mm_per_rev(float lead_screw_pitch, float gear_reduction_ratio) -> float {
+  public:
+    static constexpr auto mm_per_rev(float lead_screw_pitch,
+                                     float gear_reduction_ratio) -> float {
         return lead_screw_pitch / gear_reduction_ratio;
     }
 };
 
 class GearBoxConfig {
   public:
-    static constexpr auto mm_per_rev(float gear_diameter, float gear_reduction_ratio) -> float {
+    static constexpr auto mm_per_rev(float gear_diameter,
+                                     float gear_reduction_ratio) -> float {
         return static_cast<float>((gear_diameter * std::numbers::pi) /
                                   gear_reduction_ratio);
     }
 };
-
 
 struct LinearMotionSystemConfig {
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
@@ -36,7 +37,7 @@ struct LinearMotionSystemConfig {
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
     float microstep;
     [[nodiscard]] constexpr auto get_usteps_per_mm() const -> float {
-        return (steps_per_rev * microstep) / mm_per_rev ;
+        return (steps_per_rev * microstep) / mm_per_rev;
     }
 };
 
