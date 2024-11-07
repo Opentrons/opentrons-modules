04420b1329dfa3a93d37a684658048ec1ede9fb0
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2024-11-06 21:23:24 -0500

    simplify linear motion system config

--- stm32-modules/include/common/core/linear_motion_system.hpp
+++ stm32-modules/include/common/core/linear_motion_system.hpp
@@ -5,49 +5,33 @@
 
 namespace lms {
 
-struct BeltConfig {
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float pulley_diameter;  // mm
-    [[nodiscard]] constexpr auto get_mm_per_rev() const -> float {
+class BeltConfig {
+    float mm_per_rev(float pulley_diameter) {
         return static_cast<float>(pulley_diameter * std::numbers::pi);
     }
 };
 
-struct LeadScrewConfig {
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float lead_screw_pitch;  // mm/rev
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float gear_reduction_ratio;  // large teeth / small teeth
-    [[nodiscard]] constexpr auto get_mm_per_rev() const -> float {
+class LeadScrewConfig {
+    float mm_per_rev(float lead_screw_pitch, float gear_reduction_ratio) {
         return lead_screw_pitch / gear_reduction_ratio;
     }
 };
 
-struct GearBoxConfig {
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float gear_diameter;  // mm
-    // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float gear_reduction_ratio;  // large teeth / small teeth
-    [[nodiscard]] constexpr auto get_mm_per_rev() const -> float {
+class GearBoxConfig {
+    float mm_per_rev(float gear_diameter, float gear_reduction_ratio) {
         return static_cast<float>((gear_diameter * std::numbers::pi) /
                                   gear_reduction_ratio);
     }
 };
 
-template <typename MC>
-concept MotorMechanicalConfig = requires {
-    std::is_same_v<MC, BeltConfig> || std::is_same_v<MC, LeadScrewConfig> ||
-        std::is_same_v<MC, GearBoxConfig>;
-};
 
-template <MotorMechanicalConfig MEConfig>
 struct LinearMotionSystemConfig {
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    MEConfig mech_config{};
+    float mm_per_rev;
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float steps_per_rev{};
+    float steps_per_rev;
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
-    float microstep{};
+    float microstep;
     [[nodiscard]] constexpr auto get_usteps_per_mm() const -> float {
         return (steps_per_rev * microstep) / (mech_config.get_mm_per_rev());
     }
