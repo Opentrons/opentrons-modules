5afa00c9be4bc2c257de4ecb04f77959af7a742d
ahiuchingau <20424172+ahiuchingau@users.noreply.github.com>
2024-11-06 23:30:29 -0500

    update funcs

--- stm32-modules/include/common/core/linear_motion_system.hpp
+++ stm32-modules/include/common/core/linear_motion_system.hpp
@@ -6,19 +6,22 @@
 namespace lms {
 
 class BeltConfig {
-    float mm_per_rev(float pulley_diameter) {
+  public:
+    static constexpr auto mm_per_rev(float pulley_diameter) -> float {
         return static_cast<float>(pulley_diameter * std::numbers::pi);
     }
 };
 
 class LeadScrewConfig {
-    float mm_per_rev(float lead_screw_pitch, float gear_reduction_ratio) {
+    public:
+    static constexpr auto mm_per_rev(float lead_screw_pitch, float gear_reduction_ratio) -> float {
         return lead_screw_pitch / gear_reduction_ratio;
     }
 };
 
 class GearBoxConfig {
-    float mm_per_rev(float gear_diameter, float gear_reduction_ratio) {
+  public:
+    static constexpr auto mm_per_rev(float gear_diameter, float gear_reduction_ratio) -> float {
         return static_cast<float>((gear_diameter * std::numbers::pi) /
                                   gear_reduction_ratio);
     }
@@ -33,17 +36,7 @@ struct LinearMotionSystemConfig {
     // NOLINTNEXTLINE(misc-non-private-member-variables-in-classes)
     float microstep;
     [[nodiscard]] constexpr auto get_usteps_per_mm() const -> float {
-        return (steps_per_rev * microstep) / (mech_config.get_mm_per_rev());
-    }
-    [[nodiscard]] constexpr auto get_usteps_per_um() const -> float {
-        return (steps_per_rev * microstep) /
-               // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-               (mech_config.get_mm_per_rev() * 1000.0);
-    }
-    [[nodiscard]] constexpr auto get_um_per_step() const -> float {
-        return (mech_config.get_mm_per_rev()) / (steps_per_rev * microstep) *
-               // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers)
-               1000;
+        return (steps_per_rev * microstep) / mm_per_rev ;
     }
 };
 
