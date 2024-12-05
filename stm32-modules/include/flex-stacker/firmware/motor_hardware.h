6989863fb9d18b8af3007656b42939c9d9ae2718
caila-marashaj <caila.marashaj@opentrons.com>
2024-12-05 17:25:54 -0500

    rm old definition from motor hardware

--- stm32-modules/include/flex-stacker/firmware/motor_hardware.h
+++ stm32-modules/include/flex-stacker/firmware/motor_hardware.h
@@ -26,7 +26,6 @@ bool hw_read_platform_sensor(bool direction);
 bool hw_read_estop(void);
 bool hw_is_diag0_pin(uint16_t pin);
 bool hw_is_estop_pin(uint16_t pin);
-uint16_t motor_hardware_reset_reason();
 
 #ifdef __cplusplus
 }  // extern "C"
