89de4a9a4d0565d882c00a08f5a5cfbb261fabb4
Caila Marashaj <98041399+caila-marashaj@users.noreply.github.com>
2025-05-12 14:24:32 -0400

    fix(thermocycler): clear cache after motor-related errors (#523)

--- stm32-modules/include/thermocycler-gen2/thermocycler-gen2/host_comms_task.hpp
+++ stm32-modules/include/thermocycler-gen2/thermocycler-gen2/host_comms_task.hpp
@@ -268,6 +268,7 @@ class HostCommsTask {
         std::sized_sentinel_for<InputLimit, InputIt>
     auto visit_message(const messages::ErrorMessage& msg, InputIt tx_into,
                        InputLimit tx_limit) -> InputIt {
+        ack_only_cache.clear();
         return errors::write_into_async(tx_into, tx_limit, msg.code);
     }
 
