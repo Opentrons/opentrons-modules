9de16a1e7dd14eb0acf1fa58df9842170d27ac24
caila-marashaj <caila.marashaj@opentrons.com>
2025-11-06 16:03:55 -0500

    had a change that was j a space

--- stm32-modules/vacuum-module/firmware/system/freertos_system_task.cpp
+++ stm32-modules/vacuum-module/firmware/system/freertos_system_task.cpp
@@ -4,7 +4,6 @@
 #include "firmware/firmware_tasks.hpp"
 #include "firmware/freertos_tasks.hpp"
 #include "firmware/system_policy.hpp"
-#include "firmware/vacuum_pressure_sensor_hardware.h"
 #include "task.h"
 #include "vacuum-module/system_task.hpp"
 
@@ -28,8 +27,8 @@ auto run(tasks::FirmwareTasks::QueueAggregator* aggregator) -> void {
     _queue.provide_handle(handle);
     aggregator->register_queue(_queue);
     _top_task.provide_aggregator(aggregator);
-    auto policy = SystemPolicy();
 
+    auto policy = SystemPolicy();
     while (true) {
         _top_task.run_once(policy);
     }
