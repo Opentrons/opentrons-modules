607d0f39e96b320a7b5d8d637fa284c3b20f2ffc
caila-marashaj <caila.marashaj@opentrons.com>
2026-03-02 13:35:03 -0500

    merge pressure driver w edge

--- stm32-modules/include/vacuum-module/vacuum-module/mprll0025pa00001a.hpp
+++ stm32-modules/include/vacuum-module/vacuum-module/mprll0025pa00001a.hpp
@@ -41,22 +41,18 @@ constexpr double PMAX = 25;
 constexpr double PMIN = 0;
 constexpr double PSI2MBAR = 68.9475729318;
 constexpr uint8_t PRESSURE_FRAME_LEN = 10;
-
-constexpr int PRESSURE_FILTER_BUFFER_LEN = 8;
-// pressure needs to hold for 10 seconds to be considered stable
-constexpr int PRESSURE_STATE_BUFFER_LEN = 250;
-// TODO: currently this filter acts as an unweighted moving average. Find
-// coefficient values that optimize sensor output behavior for closed-loop
-// control
-const double moving_avg_coefficient =
-    1.0 / std::pow((PRESSURE_FILTER_BUFFER_LEN - 0.75), 2);
-const std::array<double, PRESSURE_FILTER_BUFFER_LEN> FILTER = {
+constexpr int PRESSURE_BUFFER_LEN = 8;
+// These coefficients act as an EMI (Exponential Moving Average) filter.
+// Ci = alpha(1 - alpha)^i
+// clang-format off
+const std::array<double, PRESSURE_BUFFER_LEN> FILTER = {
     // 0.1756, 0.1580, 0.1422, 0.1280, 0.1152, 0.1037, 0.0933, 0.0840  // a=0.1
     0.2403, 0.1923, 0.1538, 0.1231, 0.0985, 0.0788, 0.0630, 0.0504  // a=0.2
     // 0.3133, 0.2193, 0.1535, 0.1075, 0.0752, 0.0527, 0.0369, 0.0258  // a=0.3
     // 0.5020, 0.2510, 0.1255, 0.0627, 0.0314, 0.0157, 0.0078, 0.0039  // a=0.5
     // 0.8000, 0.1600, 0.0320, 0.0064, 0.0013, 0.0003, 0.0001, 0.0000  // a=0.8
 };
+// clang-format on
 
 // Frame retry defaults
 constexpr uint8_t DEFAULT_RETRIES = 3;
@@ -81,6 +77,7 @@ class MPRLL0025PA00001 {
             _policy = policy;
             _sensor_id = sensor_id;
             is_first_read = true;
+
             // check device status
             ok = _policy->is_device_ready(device_address << 1);
         }
@@ -114,10 +111,8 @@ class MPRLL0025PA00001 {
                 // Pre-fill the buffer if this is the first read so we dont
                 // calculate the filtered pressure from 1 reading where the
                 // rest of the buffer is holding 0.
-                filter_pressure(pressure_mbar);
                 if (is_first_read) {
-                    unfiltered_pressure_mbar.fill(pressure_mbar);
-                    filtered_pressure_state_buffer_mbar.fill(pressure_mbar);
+                    unfiltered_pressure_mbar.fill(raw_pressure);
                     is_first_read = false;
                 }
                 pressure_mbar = filter_pressure(raw_pressure);
@@ -126,20 +121,6 @@ class MPRLL0025PA00001 {
         }
 
         return -1;
-    }
-
-    auto solid_state_target_pressure(double target_pressure, double tolerance)
-        -> bool {
-        // return whether all of the pressure readings in the state buffer
-        // are within the tolerance of the target pressure
-        double pressure_sample = 0;
-        for (int i = 0; i < PRESSURE_STATE_BUFFER_LEN; i++) {
-            pressure_sample = filtered_pressure_state_buffer_mbar.at(i);
-            if (std::abs(pressure_sample - target_pressure) > tolerance) {
-                return false;
-            }
-        }
-        return true;
     }
 
   private:
@@ -173,47 +154,19 @@ class MPRLL0025PA00001 {
         return pressure_psi * PSI2MBAR;
     }
 
-// <<<<<<< HEAD
-//     auto filter_pressure(double pressure_mbar) -> double {
-//         double filter_output = 0;
-//         unfiltered_pressure_mbar.at(pressure_buffer_index) = pressure_mbar;
-//         for (int i = 0; i < PRESSURE_BUFFER_LEN; i++) {
-//             const int p_index =
-//                 (pressure_buffer_index - i + PRESSURE_BUFFER_LEN) %
-//                 PRESSURE_BUFFER_LEN;
-//             filter_output +=
-//                 unfiltered_pressure_mbar.at(p_index) * FILTER.at(i);
-//         }
-//         pressure_buffer_index =
-//             (pressure_buffer_index + 1) % PRESSURE_BUFFER_LEN;
-//         return filter_output;
-// =======
-    auto filter_pressure(double input_pressure_mbar) -> double {
-        ++filtered_pressure_buffer_index %= PRESSURE_FILTER_BUFFER_LEN;
-        ++unfiltered_pressure_buffer_index %= PRESSURE_FILTER_BUFFER_LEN;
-        ++filtered_pressure_state_buffer_mbar_index %=
-            PRESSURE_STATE_BUFFER_LEN;
-
-        unfiltered_pressure_mbar.at(unfiltered_pressure_buffer_index) =
-            input_pressure_mbar;
+    auto filter_pressure(double pressure_mbar) -> double {
         double filter_output = 0;
-        double pressure_sample = 0;
-        int p_index = unfiltered_pressure_buffer_index;
-        for (int i = 0; i < PRESSURE_FILTER_BUFFER_LEN; i++) {
-            p_index = (unfiltered_pressure_buffer_index + i) %
-                      PRESSURE_FILTER_BUFFER_LEN;
-            pressure_sample = unfiltered_pressure_mbar.at(p_index);
-            for (int j = 0; j < PRESSURE_FILTER_BUFFER_LEN; j++) {
-                filter_output += pressure_sample * FILTER.at(j);
-            }
+        unfiltered_pressure_mbar.at(pressure_buffer_index) = pressure_mbar;
+        for (int i = 0; i < PRESSURE_BUFFER_LEN; i++) {
+            const int p_index =
+                (pressure_buffer_index - i + PRESSURE_BUFFER_LEN) %
+                PRESSURE_BUFFER_LEN;
+            filter_output +=
+                unfiltered_pressure_mbar.at(p_index) * FILTER.at(i);
         }
-
-        filtered_pressure_mbar.at(filtered_pressure_buffer_index) =
-            filter_output;
-        filtered_pressure_state_buffer_mbar.at(
-            filtered_pressure_state_buffer_mbar_index) = filter_output;
+        pressure_buffer_index =
+            (pressure_buffer_index + 1) % PRESSURE_BUFFER_LEN;
         return filter_output;
-// >>>>>>> 80a6636 (make larger buffer for keeping track of target pressure state)
     }
 
     Policy* _policy{nullptr};
@@ -221,16 +174,9 @@ class MPRLL0025PA00001 {
     std::array<uint8_t, PRESSURE_FRAME_LEN> WR_BUFF = {0};
     PressureSensorID _sensor_id{};
     uint8_t device_address{};
-    // TODO: filtered_pressure_state_buffer_mbar and filtered_pressure_mbar are
-    // redundant
-    std::array<double, PRESSURE_FILTER_BUFFER_LEN> filtered_pressure_mbar = {0};
-    std::array<double, PRESSURE_FILTER_BUFFER_LEN> unfiltered_pressure_mbar = {
-        0};
-    std::array<double, PRESSURE_STATE_BUFFER_LEN>
-        filtered_pressure_state_buffer_mbar = {0};
-    size_t filtered_pressure_buffer_index = 0;
-    size_t unfiltered_pressure_buffer_index = 0;
-    size_t filtered_pressure_state_buffer_mbar_index = 0;
+
+    std::array<double, PRESSURE_BUFFER_LEN> unfiltered_pressure_mbar = {0};
+    uint8_t pressure_buffer_index = 0;
     double pressure_mbar = 0;
     uint8_t last_status = 0;
     bool is_first_read = true;
