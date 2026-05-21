68f6393304beefd0457c917744dbdabb0f045a33
Seth Foster <seth@opentrons.com>
2026-05-21 10:32:54 -0400

    fix: correct reset reason logic (#560)
    The reset reason logic had a couple issues
    - Not all the RCC flags are about reset reasons; some of them are more
    in the CC than the R (of Reset and Clock Control) and are about
    monitoring the readiness of internal clock sources. Omit these
    - The logic would only emit the first positive flag it found, which in
    the current implementation was always one of the clock readiness flags.
    Instead, emit all positive flags
    - There are slightly different reset-reason flags available on the
    F3 (which is used in the heater-shaker) and the G4 (which is used
    everywhere else); adapt the code on the heater-shaker to that

--- stm32-modules/vacuum-module/firmware/system/system_hardware.c
+++ stm32-modules/vacuum-module/firmware/system/system_hardware.c
@@ -1,52 +1,39 @@
+#include "firmware/system_hardware.h"
+
+#include "main.h"
 #include "stm32g4xx_hal.h"
-#include "stm32g4xx_hal_rcc.h"
 #include "stm32g4xx_hal_cortex.h"
+#include "stm32g4xx_hal_rcc.h"
 #include "stm32g4xx_hal_tim.h"
 
-#include "firmware/system_hardware.h"
-#include "main.h"
-
 /** Local defines */
-// This is the start of the sys memory region for the STM32G491 
+// This is the start of the sys memory region for the STM32G491
 // from the reference manual and STM application note AN2606
 #define SYSMEM_START 0x1fff0000
 #define SYSMEM_BOOT (SYSMEM_START + 4)
 
 // address 4 in the bootable region is the address of the first instruction that
 // should run, aka the data that should be loaded into $pc.
-const uint32_t *const sysmem_boot_loc = (uint32_t*)SYSMEM_BOOT;
+const uint32_t *const sysmem_boot_loc = (uint32_t *)SYSMEM_BOOT;
 uint16_t reset_reason;
 
 enum RCC_FLAGS {
-    _NONE,
-    // high speed internal clock ready
-    HSIRDY, // = 1
-    // high speed external clock ready
-    HSERDY, // = 2
-    // main phase-locked loop clock ready
-    PLLRDY, // = 3
-    // hsi48 clock ready
-    HSI48RDY, // = 4
-    // low-speed external clock ready
-    LSERDY, // = 5
     // lse clock security system failure
-    LSECSSD, // = 6
-    // low-speed internal clock ready
-    LSIRDY, // = 7
+    LSECSSD,  // = 0
     // brown out
-    BORRST, // = 8
+    BORRST,  // = 1
     // option byte-loader reset
-    OBLRST, // = 9
+    OBLRST,  // = 2
     // pin reset
-    PINRST, // = 10
+    PINRST,  // = 3
     // software reset
-    SFTRST, // = 11
+    SFTRST,  // = 4
     // independent watchdog
-    IWDGRST, // = 12
+    IWDGRST,  // = 5
     // window watchdog
-    WWDGRST, // = 13
+    WWDGRST,  // = 6
     // low power reset
-    LPWRRST, // = 14
+    LPWRRST,  // = 7
 };
 
 static void save_reset_reason() {
@@ -54,76 +41,46 @@ static void save_reset_reason() {
     // reset flag matches any of them
     reset_reason = 0;
 
-    // high speed internal clock ready
-    if (__HAL_RCC_GET_FLAG(RCC_FLAG_HSIRDY)) {
-        reset_reason |= HSIRDY;
-    }
-    // high speed external clock ready
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_HSERDY)) {
-        reset_reason |= HSERDY;
-    }
-    // main phase-locked loop clock ready
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PLLRDY)) {
-        reset_reason |= PLLRDY;
-    }
-    // hsi48 clock ready
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_HSIRDY)) {
-        reset_reason |= HSI48RDY;
-    }
-    // low-speed external clock ready
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_LSERDY)) {
-        reset_reason |= LSERDY;
-    }
-    // lse clock security system failure
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_LSERDY)) {
-        reset_reason |= LSECSSD;
-    }
-    // low-speed internal clock ready
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_LSIRDY)) {
-        reset_reason |= LSIRDY;
-    }
     // brown out
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_BORRST)) {
-        reset_reason |= BORRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_BORRST)) {
+        reset_reason |= (1 << BORRST);
     }
     // option byte-loader reset
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_OBLRST)) {
-        reset_reason |= OBLRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_OBLRST)) {
+        reset_reason |= (1 << OBLRST);
     }
     // pin reset
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST)) {
-        reset_reason |= PINRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST)) {
+        reset_reason |= (1 << PINRST);
     }
     // software reset
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST)) {
-        reset_reason |= SFTRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST)) {
+        reset_reason |= (1 << SFTRST);
     }
     // independent watchdog
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) {
-        reset_reason |= IWDGRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST)) {
+        reset_reason |= (1 << IWDGRST);
     }
     // window watchdog
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST)) {
-        reset_reason |= WWDGRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST)) {
+        reset_reason |= (1 << WWDGRST);
     }
     // low power reset
-    else if (__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST)) {
-        reset_reason |= LPWRRST;
+    if (__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST)) {
+        reset_reason |= (1 << LPWRRST);
     }
 }
 
-uint16_t system_hardware_reset_reason() {
-    return reset_reason;
-}
+uint16_t system_hardware_reset_reason() { return reset_reason; }
 
 /** PUBLIC FUNCTION IMPLEMENTATION */
 
 void system_hardware_enter_bootloader(void) {
+    // We have to uninitialize as many of the peripherals as possible, because
+    // the bootloader expects to start as the system comes up
 
-    // We have to uninitialize as many of the peripherals as possible, because the bootloader
-    // expects to start as the system comes up
-
-    // The HAL has ways to turn off all the core clocking and the clock security system
+    // The HAL has ways to turn off all the core clocking and the clock security
+    // system
     HAL_RCC_DisableLSECSS();
     HAL_RCC_DeInit();
 
@@ -133,32 +90,31 @@ void system_hardware_enter_bootloader(void) {
     SysTick->VAL = 0;
 
     /* Clear Interrupt Enable Register & Interrupt Pending Register */
-    for (int i=0;i<8;i++)
-    {
-        NVIC->ICER[i]=0xFFFFFFFF;
-        NVIC->ICPR[i]=0xFFFFFFFF;
+    for (int i = 0; i < 8; i++) {
+        NVIC->ICER[i] = 0xFFFFFFFF;
+        NVIC->ICPR[i] = 0xFFFFFFFF;
     }
 
-    // We have to make sure that the processor is mapping the system memory region to address 0,
-    // which the bootloader expects
+    // We have to make sure that the processor is mapping the system memory
+    // region to address 0, which the bootloader expects
     __HAL_SYSCFG_REMAPMEMORY_SYSTEMFLASH();
     // and now we're ready to set the system up to start executing system flash.
     // arm cortex initialization means that
-    // address 0 in the bootable region is the address where the processor should start its stack
-    // which we have to do as late as possible because as soon as we do this the c and c++ runtime
-    // environment is no longer valid
-    __set_MSP(*((uint32_t*)SYSMEM_START));
+    // address 0 in the bootable region is the address where the processor
+    // should start its stack which we have to do as late as possible because as
+    // soon as we do this the c and c++ runtime environment is no longer valid
+    __set_MSP(*((uint32_t *)SYSMEM_START));
 
     // finally, jump to the bootloader. we do this in inline asm because we need
-    // this to be a naked call (no caller-side prep like stacking return addresses)
-    // and to have a naked function you need to define it as a function, not a
-    // function pointer, and we don't statically know the address here since it is
-    // whatever's contained in that second word of the bsystem memory region.
-    asm volatile (
-        "bx %0"
-        : // no outputs
-        : "r" (*sysmem_boot_loc)
-        : "memory"  );
+    // this to be a naked call (no caller-side prep like stacking return
+    // addresses) and to have a naked function you need to define it as a
+    // function, not a function pointer, and we don't statically know the
+    // address here since it is whatever's contained in that second word of the
+    // bsystem memory region.
+    asm volatile("bx %0"
+                 :  // no outputs
+                 : "r"(*sysmem_boot_loc)
+                 : "memory");
 }
 
 /**
@@ -181,7 +137,8 @@ void eeprom_write_protect_init(void) {
  * enable/disable writing to the eeprom.
  */
 void enable_eeprom_write(bool enable) {
-    HAL_GPIO_WritePin(EEPROM_WP_PORT, EEPROM_WP_PIN, enable ? GPIO_PIN_RESET : GPIO_PIN_SET);
+    HAL_GPIO_WritePin(EEPROM_WP_PORT, EEPROM_WP_PIN,
+                      enable ? GPIO_PIN_RESET : GPIO_PIN_SET);
 }
 
 void system_hardware_gpio_init(void) {
@@ -190,4 +147,3 @@ void system_hardware_gpio_init(void) {
     // Disable eeprom write
     enable_eeprom_write(false);
 }
-
